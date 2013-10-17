d20e61ad82c5d17ac86577eee499670ba1de25a6
Edd Cawley <edd@azquo.com>
2013-10-17 15:03:44 +0000

    trying to sort libraries for testing
    git-svn-id: http://192.168.1.10/repos/toto@9 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/test/com/azquo/toto/dao/StandardDAOTest.java
@@ -0,0 +1,70 @@
+package com.azquo.toto.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cawley
+ * Date: 17/10/13
+ * Time: 15:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:totospringdispatcher-servlet.xml"})
+
+public class StandardDAOTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateById() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+/*    @Test
+    public void testInsert() throws Exception {
+
+    }*/
+
+    @Test
+    public void testStore() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+
+    @Test
+    public void testFindListWithWhereSQLAndParameters() throws Exception {
+
+    }
+
+    @Test
+    public void testFindOneWithWhereSQLAndParameters() throws Exception {
+
+    }
+}
