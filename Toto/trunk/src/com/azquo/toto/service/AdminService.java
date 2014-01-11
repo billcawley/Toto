d30c1902c789eb9541cab9bbfff03a5881aa86b7
Edd Cawley <edd@azquo.com>
2014-01-11 00:57:00 +0000

    more tidying
    git-svn-id: http://192.168.1.10/repos/toto@155 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/toto/service/AdminService.java
+++ Toto/trunk/src/com/azquo/toto/service/AdminService.java
@@ -5,8 +5,6 @@ import com.azquo.toto.adminentities.Access;
 import com.azquo.toto.adminentities.Business;
 import com.azquo.toto.adminentities.Database;
 import com.azquo.toto.adminentities.User;
-import com.google.gson.Gson;
-import com.sun.accessibility.internal.resources.accessibility_de;
 import org.springframework.beans.factory.annotation.Autowired;
 import sun.misc.BASE64Encoder;
 
@@ -36,19 +34,20 @@ public class AdminService {
     @Autowired
     MySQLDatabaseManager mySQLDatabaseManager;
 
-    public String registerBusiness(String email, String userName, String password, String businessName, String address1, String address2, String address3, String address4, String postcode, String telephone){
-        String key = shaHash(System.currentTimeMillis() + "");
-        Business.BusinessDetails bd = new Business.BusinessDetails(address1,address2,address3,address4,postcode, telephone,"website???", key);
-        Business business = new Business(0,false,new Date(), businessName,0, bd);
+    public String registerBusiness(final String email, final String userName, final String password, final String businessName, final String address1
+            , final String address2, final String address3, final String address4, final String postcode, final String telephone){
+        final String key = shaHash(System.currentTimeMillis() + "");
+        final Business.BusinessDetails bd = new Business.BusinessDetails(address1,address2,address3,address4,postcode, telephone,"website???", key);
+        final Business business = new Business(0,false,new Date(), businessName,0, bd);
         businessDao.store(business);
-        String salt = shaHash(System.currentTimeMillis() + "salt");
-        User user = new User(0, false, new Date(),business.getId(), email, userName, "registered", encrypt(password, salt), salt);
+        final String salt = shaHash(System.currentTimeMillis() + "salt");
+        final User user = new User(0, false, new Date(),business.getId(), email, userName, "registered", encrypt(password, salt), salt);
         userDao.store(user);
         return key;
     }
 
-    public boolean confirmKey(String businessName, String key){
-        Business business = businessDao.findByName(businessName);
+    public boolean confirmKey(final String businessName, final String key){
+        final Business business = businessDao.findByName(businessName);
         if (business != null && business.getBusinessDetails().getValidationKey().equals(key)){
             business.setActive(true);
             businessDao.store(business);
@@ -61,7 +60,7 @@ public class AdminService {
         return false;
     }
 
-    public String getSQLDatabaseName(LoggedInConnection loggedInConnection, String databaseName) {
+    public String getSQLDatabaseName(final LoggedInConnection loggedInConnection, final String databaseName) {
         Business b = businessDao.findById(loggedInConnection.getUser().getBusinessId());
         String mysqlName = b.getBusinessName().substring(0,5) + "_" + databaseName;
         return mysqlName.replaceAll("[^A-Za-z0-9]", "");
@@ -69,12 +68,12 @@ public class AdminService {
 
     }
 
-    public boolean createDatabase(String databaseName, LoggedInConnection loggedInConnection) throws IOException {
+    public boolean createDatabase(final String databaseName, final LoggedInConnection loggedInConnection) throws IOException {
 
         if (loggedInConnection.getUser().isAdministrator()){
-            String mysqlName = getSQLDatabaseName(loggedInConnection,databaseName);
-            Business b = businessDao.findById(loggedInConnection.getUser().getBusinessId());
-            Database database = new Database(0,true, new Date(), b.getId(), databaseName,mysqlName,0,0);
+            final String mysqlName = getSQLDatabaseName(loggedInConnection,databaseName);
+            final Business b = businessDao.findById(loggedInConnection.getUser().getBusinessId());
+            final Database database = new Database(0,true, new Date(), b.getId(), databaseName,mysqlName,0,0);
             mySQLDatabaseManager.createNewDatabase(mysqlName);
             databaseDao.store(database);
             return true;
@@ -83,20 +82,20 @@ public class AdminService {
         return false;
     }
 
-    public boolean createUser(String email, String userName, String status,String password, LoggedInConnection loggedInConnection) throws IOException {
+    public boolean createUser(final String email, final String userName, final String status, final String password, final LoggedInConnection loggedInConnection) throws IOException {
 
         if (loggedInConnection.getUser().isAdministrator()){
-            String salt = shaHash(System.currentTimeMillis() + "salt");
-            User user = new User(0, true, new Date(),loggedInConnection.getUser().getBusinessId(), email, userName, status, encrypt(password, salt), salt);
+            final String salt = shaHash(System.currentTimeMillis() + "salt");
+            final User user = new User(0, true, new Date(),loggedInConnection.getUser().getBusinessId(), email, userName, status, encrypt(password, salt), salt);
             userDao.store(user);
             return true;
         }
         return false;
     }
 
-    public boolean createUserAccess(String email, String readList,String writeList, LoggedInConnection loggedInConnection) throws IOException {
+    public boolean createUserAccess(final String email, final String readList, final String writeList, final LoggedInConnection loggedInConnection) throws IOException {
         if (loggedInConnection.getUser().isAdministrator() && loggedInConnection.getTotoMemoryDB() != null){ // actually have a DB selected
-            Access access = new Access(0, true, new Date(), userDao.findByEmail(email).getId(), loggedInConnection.getTotoMemoryDB().getDatabase().getId(), readList,writeList);
+            final Access access = new Access(0, true, new Date(), userDao.findByEmail(email).getId(), loggedInConnection.getTotoMemoryDB().getDatabase().getId(), readList,writeList);
             accessDao.store(access);
             return true;
         }
@@ -110,7 +109,7 @@ public class AdminService {
     v.setPassword(PasswordUtils.encrypt(password, salt)); // new better encryption . . .*/
 
 
-    public static String encrypt(String password, String salt)
+    public static String encrypt(final String password, String salt)
     {
         try {
             // WARNING! DO NOT MODIFY THE reference to "scapbadopbebedop"  bit in the code below or it will stop working! This is the extra bit on the salt or the number of hash cycles! stay at 3296
@@ -118,8 +117,7 @@ public class AdminService {
             byte[] passBytes = password.getBytes();
             byte[] saltBytes = salt.getBytes();
             int hashCycles = 3296;
-            MessageDigest md = null;
-            md = MessageDigest.getInstance("SHA");
+            MessageDigest md = MessageDigest.getInstance("SHA");
             for (int i = 0; i < hashCycles; i++){
                 // add the salt to the pass and encrypt, then add the salt to the result and encrypt. Repeat as necessary . . .
                 byte[] combined = new byte[saltBytes.length + passBytes.length];
@@ -130,15 +128,14 @@ public class AdminService {
                 md.update(combined);
                 passBytes = md.digest();
             }
-            String result = (new BASE64Encoder()).encode(passBytes);
-            return result;
+            return (new BASE64Encoder()).encode(passBytes);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "fail . . .";
     }
 
-    public static String shaHash(String toHash){   // for making a password salt
+    public static String shaHash(final String toHash){   // for making a password salt
         try {
             java.security.MessageDigest md = java.security.MessageDigest.getInstance("SHA-1");
             byte[] hash = md.digest(toHash.getBytes());
@@ -154,21 +151,21 @@ public class AdminService {
     }
 
 
-    public List<Database> getDatabaseListForBusiness(LoggedInConnection loggedInConnection) {
+    public List<Database> getDatabaseListForBusiness(final LoggedInConnection loggedInConnection) {
         if (loggedInConnection.getUser().isAdministrator()){
             return databaseDao.findForBusinessId(loggedInConnection.getUser().getBusinessId());
         }
         return null;
     }
 
-    public List<User> getUserListForBusiness(LoggedInConnection loggedInConnection) {
+    public List<User> getUserListForBusiness(final LoggedInConnection loggedInConnection) {
         if (loggedInConnection.getUser().isAdministrator()){
             return userDao.findForBusinessId(loggedInConnection.getUser().getBusinessId());
         }
         return null;
     }
 
-    public List<Access> getAccessList(LoggedInConnection loggedInConnection, String userEmail) {
+    public List<Access> getAccessList(final LoggedInConnection loggedInConnection, String userEmail) {
         if (loggedInConnection.getUser().isAdministrator()){
             User user = userDao.findByEmail(userEmail);
             if (user != null && user.getBusinessId() == loggedInConnection.getUser().getBusinessId()){ // we can show access for that user
--- Toto/trunk/src/com/azquo/toto/service/ImportService.java
+++ Toto/trunk/src/com/azquo/toto/service/ImportService.java
@@ -1,6 +1,5 @@
 package com.azquo.toto.service;
 
-import com.azquo.toto.controller.NameController;
 import com.azquo.toto.memorydb.Name;
 import com.azquo.toto.memorydb.Provenance;
 import com.csvreader.CsvReader;
@@ -13,30 +12,19 @@ import java.util.zip.ZipInputStream;
 
 /**
  * Created by bill on 13/12/13.
+ * service to process files used to import data into the database
  */
 
-
-
-
 public final class ImportService {
-
-
 
     @Autowired
     private ValueService valueService;
     @Autowired
     private NameService nameService;
-    @Autowired
-    private ProvenanceService provenanceService;
 
-     public String dataImport(LoggedInConnection loggedInConnection, String fileName, String language, boolean create) throws Exception {
-
-
-         // OK I think I'm supposed to use language in here but how??? Will go to default name for the moment
-
-         HashMap<Name, String> nameImportHeadingMap = new HashMap<Name, String>();
-
-
+    public String dataImport(final LoggedInConnection loggedInConnection, final String fileName, final String language, final boolean create) throws Exception {
+        // OK I think I'm supposed to use language in here but how??? Will go to default name for the moment
+        final HashMap<Name, String> nameImportHeadingMap = new HashMap<Name, String>();
         //String filePath = "/home/bill/Downloads/exportcodes.csv";
         //TODO  set correct filepath
         String filePath = "/home/cawley/Downloads/" + fileName;
@@ -49,62 +37,60 @@ public final class ImportService {
         if (create) strCreate = ";create";
 
 
-
-        CsvReader csvReader = new CsvReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"), '\t');
+        final CsvReader csvReader = new CsvReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"), '\t');
         csvReader.readHeaders();
-        String[] headers = csvReader.getHeaders();
+        final String[] headers = csvReader.getHeaders();
         // are the following few lines necessary??
-        ArrayList<Name> headerNames = new ArrayList<Name>();
+        final ArrayList<Name> headerNames = new ArrayList<Name>();
 
-        for(String header : headers){
-            if (header.trim().length() > 0){ // I don't know if the csv reader checks for this
-                 String result = nameService.handleRequest(loggedInConnection,header + strCreate);
-                if (result.startsWith("error:")){
+        for (String header : headers) {
+            if (header.trim().length() > 0) { // I don't know if the csv reader checks for this
+                final String result = nameService.handleRequest(loggedInConnection, header + strCreate);
+                if (result.startsWith("error:")) {
                     throw new Exception("unable to understand " + header + " - " + result);
                 }
                 String nameToFind = header;
-                if (header.contains(";")) nameToFind = header.substring(0,header.indexOf(";"));
-                Name name = nameService.findOrCreateName(loggedInConnection, nameToFind);
-                if (name.getPeers().size() > 0){
+                if (header.contains(";")) nameToFind = header.substring(0, header.indexOf(";"));
+                final Name name = nameService.findOrCreateName(loggedInConnection, nameToFind);
+                if (name.getPeers().size() > 0) {
                     nameImportHeadingMap.put(name, header);
                     headerNames.add(name);
                 }
             }
         }
 
-        if (headerNames.size() == 0){
-            throw new Exception("unable to find any name with peers" );
+        if (headerNames.size() == 0) {
+            throw new Exception("unable to find any name with peers");
         }
 
 
-
-        Provenance provenance = loggedInConnection.getProvenance();
+        final Provenance provenance = loggedInConnection.getProvenance();
         int valuecount = 0;
-        HashMap<String, Name> namesFound = new HashMap<String, Name>();
-        while (csvReader.readRecord()){
+        final HashMap<String, Name> namesFound = new HashMap<String, Name>();
+        while (csvReader.readRecord()) {
             String value;
-            for (Name headerName : headerNames){
-                Set<Name> namesForValue = new HashSet<Name>();
+            for (Name headerName : headerNames) {
+                final Set<Name> namesForValue = new HashSet<Name>();
                 namesForValue.add(headerName);
-                for (Name peer : headerName.getPeers().keySet()){
-                    String peerVal = csvReader.get(peer.getDefaultDisplayName());
-                    if (peerVal == null || peerVal.length() == 0){
+                for (Name peer : headerName.getPeers().keySet()) {
+                    final String peerVal = csvReader.get(peer.getDefaultDisplayName());
+                    if (peerVal == null || peerVal.length() == 0) {
                         //throw new Exception("unable to find " + peer.getName() + " for " + headerName.getName());
-                    }else{
+                    } else {
                         //storeStructuredName(peer,peerVal, loggedInConnection);
-                        String nameToFind =  peerVal + "," + peer.getDefaultDisplayName();
+                        final String nameToFind = peerVal + "," + peer.getDefaultDisplayName();
                         Name nameFound = namesFound.get(nameToFind);
-                        if (nameFound==null){
-                            if (create){
-                                nameFound = nameService.findOrCreateName(loggedInConnection,nameToFind);
-                            }else{
-                                nameFound = nameService.findByName(loggedInConnection,nameToFind);
+                        if (nameFound == null) {
+                            if (create) {
+                                nameFound = nameService.findOrCreateName(loggedInConnection, nameToFind);
+                            } else {
+                                nameFound = nameService.findByName(loggedInConnection, nameToFind);
                             }
-                            if (nameFound!=null){
+                            if (nameFound != null) {
                                 namesFound.put(nameToFind, nameFound);
                             }
                         }
-                        if (nameFound != null){
+                        if (nameFound != null) {
                             namesForValue.add(nameFound);
                         }
                     }
@@ -113,10 +99,10 @@ public final class ImportService {
 
                 }
                 value = csvReader.get(nameImportHeadingMap.get(headerName));
-                if (value.trim().length() > 0){ // no point storing if there's no value!
+                if (value.trim().length() > 0) { // no point storing if there's no value!
                     valuecount++;
                     valueService.storeValueWithProvenanceAndNames(loggedInConnection, value, provenance, namesForValue);
-                    if (valuecount%5000 == 0){
+                    if (valuecount % 5000 == 0) {
                         System.out.println("storing value " + valuecount);
                     }
                 }
@@ -131,115 +117,102 @@ public final class ImportService {
     }
 
 
-  public String attributeImport(LoggedInConnection loggedInConnection, String fileName, String language, boolean create) throws Exception {
+    public String attributeImport(final LoggedInConnection loggedInConnection, final String fileName, final String language, final boolean create) throws Exception {
 
-    //String filePath = "/home/bill/Downloads/exportcodes.csv";
-    //TODO  set correct filepath
-    String filePath = "/home/cawley/Downloads/" + fileName;
-    if (filePath.endsWith(".zip")) {
-        filePath = unzip(filePath);
-    }
-    CsvReader csvReader = new CsvReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"), '\t');
-    csvReader.readHeaders();
-    String[] headers = csvReader.getHeaders();
+        //String filePath = "/home/bill/Downloads/exportcodes.csv";
+        //TODO  set correct filepath
+        String filePath = "/home/cawley/Downloads/" + fileName;
+        if (filePath.endsWith(".zip")) {
+            filePath = unzip(filePath);
+        }
+        final CsvReader csvReader = new CsvReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"), '\t');
+        csvReader.readHeaders();
+        final String[] headers = csvReader.getHeaders();
 
 
-     while (csvReader.readRecord()){
-        String searchName = csvReader.get(language);
-        Name name = null;
+        while (csvReader.readRecord()) {
+            final String searchName = csvReader.get(language);
+            Name name = null;
 
-         // ok this is going to search for things in the language column with the default name . . doesn't really make sense!
+            // ok this is going to search for things in the language column with the default name . . doesn't really make sense!
 
-        if (searchName != null){
-            if (create){
-                name = nameService.findOrCreateName(loggedInConnection, searchName);
-            }else{
-                name = nameService.findByName(loggedInConnection,searchName);
+            if (searchName != null) {
+                if (create) {
+                    name = nameService.findOrCreateName(loggedInConnection, searchName);
+                } else {
+                    name = nameService.findByName(loggedInConnection, searchName);
+                }
+            }
+            if (name != null) {
+                for (String header : headers) {
+                    if (header.length() > 0) {
+                        final String newName = getFirstName(csvReader.get(header));
+                        final String oldName = name.getAttribute(header);
+                        if ((oldName == null && newName.length() > 0) || (oldName != null && !newName.equals(oldName))) {
+                            if (newName.length() == 0) {
+                                name.removeAttributeWillBePersisted(header);
+                            } else {
+                                name.setAttributeWillBePersisted(header, getFirstName(newName));
+                            }
+                            name.setEntityColumnsChanged();// may be overdoing it if the attributes do not affect the current name
+                        }
+                    }
+                }
+
             }
         }
-        if (name != null){
-            for (String header: headers){
-              if (header.length() > 0){
-                  String newName = getFirstName(csvReader.get(header));
-                  String oldName = name.getAttribute(header);
-                   if ((oldName == null && newName.length() > 0) || (oldName != null && !newName.equals(oldName))){
-                      if (newName.length()==0){
-                          name.removeAttributeWillBePersisted(header);
-                      }else{
-                        name.setAttributeWillBePersisted(header, getFirstName(newName));
-                      }
-                      name.setEntityColumnsChanged();// may be overdoing it if the attributes do not affect the current name
-                  }
-              }
-            }
-
-        }
+        return "";
     }
 
+    // I'm guessing from the internet?
 
-    return "";
-}
-
-    public String unzip(String zipFile){
+    public String unzip(final String zipFile) {
 
         byte[] buffer = new byte[1024];
         File newFile = null;
-        try{
-
-            String outputFolder = zipFile.substring(0, zipFile.lastIndexOf(File.separator));
+        try {
+            final String outputFolder = zipFile.substring(0, zipFile.lastIndexOf(File.separator));
             //get the zip file content
-            ZipInputStream zis =
-                    new ZipInputStream(new FileInputStream(zipFile));
+            final ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
             //get the zipped file list entry
             ZipEntry ze = zis.getNextEntry();
 
-            while(ze!=null){
-
-                String fileName = ze.getName();
+            while (ze != null) {
+                final String fileName = ze.getName();
                 newFile = new File(outputFolder + File.separator + fileName);
-
-                System.out.println("file unzip : "+ newFile.getAbsoluteFile());
-
+                System.out.println("file unzip : " + newFile.getAbsoluteFile());
                 //create all non exists folders
                 //else you will hit FileNotFoundException for compressed folder
                 new File(newFile.getParent()).mkdirs();
-
-                FileOutputStream fos = new FileOutputStream(newFile);
-
+                final FileOutputStream fos = new FileOutputStream(newFile);
                 int len;
                 while ((len = zis.read(buffer)) > 0) {
                     fos.write(buffer, 0, len);
                 }
-
                 fos.close();
                 ze = zis.getNextEntry();
             }
-
             zis.closeEntry();
             zis.close();
-
             System.out.println(zipFile + " Unzipped to " + newFile.getName());
-
-        }catch(IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
         return newFile.getPath();
     }
 
 
-    private String getFirstName(String nameGiven){
+    private String getFirstName(final String nameGiven) {
         if (nameGiven == null) return null;
         int commaPos = nameGiven.indexOf(",");
         if (commaPos < 0) return nameGiven;
         int quotePos = nameGiven.indexOf("`");
-        if (quotePos < commaPos && quotePos >= 0){
+        if (quotePos < commaPos && quotePos >= 0) {
             int endQuote = nameGiven.indexOf("`", quotePos + 1);
             return nameGiven.substring(quotePos + 1, endQuote);
         }
         return nameGiven.substring(0, commaPos);
     }
-
-
 
 
 }
