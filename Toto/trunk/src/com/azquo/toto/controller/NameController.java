d6a1f875707a58d7f72a3fdbe6562d35c1d20297
Edd Cawley <edd@azquo.com>
2014-01-24 09:29:15 +0000

    chopped down to just json
    git-svn-id: http://192.168.1.10/repos/toto@205 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/toto/controller/NameController.java
+++ Toto/trunk/src/com/azquo/toto/controller/NameController.java
@@ -7,6 +7,15 @@ package com.azquo.toto.controller;
  * Time: 11:41
  * We're going to try for spring annotation based controllers. Might look into some rest specific spring stuff later.
  * For the moment it parses instructions for manipulating the name set and calls the name service if the instructions seem correctly formed.
+ *
+ * OK, 23rd Jan here are the calls the main excel sheet is currently doing:
+ *
+ * All Json!
+ * structure
+ * edit
+ *
+ *
+ *
  */
 
 import com.azquo.toto.jsonrequestentities.NameJsonRequest;
@@ -21,20 +30,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-
 @Controller
 @RequestMapping("/Name")
 public class NameController {
 
     private static final ObjectMapper jacksonMapper = new ObjectMapper();
 
-
-    /*public static final String LEVEL = "level";
-    public static final String FROM = "from";
-    public static final String TO = "to";
-    public static final String SORTED = "sorted";*/
-    //public static final String LOWEST = "lowest";
 
     @Autowired
     private NameService nameService;
@@ -47,31 +48,9 @@ public class NameController {
 
     @RequestMapping
     @ResponseBody
-    public String handleRequest(@RequestParam(value = "connectionid", required = false) String connectionId, @RequestParam(value = "instructions", required = false) String instructions,
-                                @RequestParam(value = "jsonfunction", required = false) String jsonfunction, @RequestParam(value = "user", required = false) String user,
-                                @RequestParam(value = "password", required = false) String password, @RequestParam(value = "database", required = false) String database,
-                                @RequestParam(value = "json", required = false) String json) throws Exception {
-/*        NameJsonRequest testjson = new NameJsonRequest();
-        testjson.user = "username";
-        testjson.password = "password34234";
-        testjson.database = "database4343";
-        testjson.operation = "operation13544235";
-        testjson.connectionId = "2";
-        testjson.jsonFunction = "here is the json function";
-        testjson.name = "here is a name";
-        testjson.oldParent = 1;
-        testjson.newParent = 2;
-        testjson.newPosition = 4;
-        HashMap<String, String> test = new HashMap<String, String>();
-        test.put("attribute 1", "attribute 1 value");
-        test.put("attribute 2", "attribute 2 value");
-        test.put("attribute 3", "attribute 3 value");
-        testjson.attributes = test;
-        testjson.withData = true;
-        System.out.println("json test : " + jacksonMapper.writeValueAsString(testjson));
-        System.out.println("json back : " + jacksonMapper.writeValueAsString(jacksonMapper.readValue(jacksonMapper.writeValueAsString(testjson), NameJsonRequest.class)));*/
+    public String handleRequest(@RequestParam(value = "json", required = true) String json) throws Exception {
         try {
-            if (json != null && json.length() > 0){ // new style paramters sent as json
+            if (json.length() > 0){ // new style paramters sent as json
                 NameJsonRequest nameJsonRequest;
                 try{
                     nameJsonRequest = jacksonMapper.readValue(json, NameJsonRequest.class);
@@ -84,20 +63,8 @@ public class NameController {
                 }
                 String result = nameService.processJsonRequest(loggedInConnection, nameJsonRequest);
                 return nameJsonRequest.jsonFunction != null && nameJsonRequest.jsonFunction.length() > 0 ? nameJsonRequest.jsonFunction + "(" + result + ")" : result;
-            } else { // old style
-                if (connectionId == null) {
-                    LoggedInConnection loggedInConnection = loginService.login(database,user, password,0);
-                    if (loggedInConnection == null){
-                        return "error:no connection id";
-                    }
-                    connectionId = loggedInConnection.getConnectionId();
-                }
-                final LoggedInConnection loggedInConnection = loginService.getConnection(connectionId);
-                if (loggedInConnection == null) {
-                    return "error:invalid or expired connection id";
-                }
-                String result = nameService.handleRequest(loggedInConnection, instructions);
-                return jsonfunction != null && jsonfunction.length() > 0 ? jsonfunction + "(" + result + ")" : result;
+            } else {
+                return "error: empty json string passed";
             }
         }catch(Exception e){
             e.printStackTrace();
