82cfcbf8d4048908b72dddb72660adaf0111f641
Bill Cawley <bill@azquo.com>
2014-01-24 11:50:36 +0000

    with zipped and xls under development
    git-svn-id: http://192.168.1.10/repos/toto@211 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/toto/service/ImportService.java
+++ Toto/trunk/src/com/azquo/toto/service/ImportService.java
@@ -1,14 +1,25 @@
 package com.azquo.toto.service;
 
+import com.azquo.toto.admindao.UploadRecordDAO;
+import com.azquo.toto.adminentities.Database;
+import com.azquo.toto.adminentities.UploadRecord;
 import com.azquo.toto.memorydb.Name;
 import com.azquo.toto.memorydb.Provenance;
+import com.azquo.toto.memorydb.TotoMemoryDB;
 import com.csvreader.CsvReader;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -24,6 +35,60 @@ public final class ImportService {
     private ValueService valueService;
     @Autowired
     private NameService nameService;
+    @Autowired
+    private UploadRecordDAO uploadRecordDAO;
+
+
+    public String importTheFile(final LoggedInConnection loggedInConnection, String fileName, InputStream uploadFile, String fileType, String separator, final String strCreate)
+            throws Exception{
+
+        if (separator == null || separator.length() == 0) separator = "\t";
+        if (separator.equals("comma")) separator = ",";
+        // separator not used??
+        if (separator.equals("pipe")) separator = "|";
+        String result = "";
+        String tempFile = "";
+        boolean create = false;
+        if (strCreate != null && strCreate.equals("true")){
+            create = true;
+        }
+        if (fileName.endsWith(".xsl")){
+            tempFile = decode64(uploadFile,fileName);
+        }
+        if (fileName.endsWith(".zip")) {
+            tempFile = decode64(uploadFile, fileName);
+            fileName = fileName.substring(0, fileName.length() - 4);
+
+            tempFile = unzip(tempFile, fileName.substring(fileName.length() - 4));
+
+
+        }
+        if (fileName.contains(".xls")) {
+            readExcel(fileName);
+
+        }else{
+            if (tempFile.length() > 0){
+               uploadFile = new FileInputStream(tempFile);
+            }
+            if (fileType.toLowerCase().equals("values")){
+                result =  dataImport(loggedInConnection,  uploadFile, create);
+            }
+            // we will pay attention onn the attribute import and replicate
+            if (fileType.toLowerCase().equals("names")){
+                result = attributeImport(loggedInConnection,uploadFile, create);
+
+            }
+        }
+        nameService.persist(loggedInConnection);
+        Database db = loggedInConnection.getTotoMemoryDB().getDatabase();
+        UploadRecord uploadRecord = new UploadRecord(0,new Date(),db.getBusinessId(), db.getId(), loggedInConnection.getUser().getId(),fileName, fileType, result);
+        uploadRecordDAO.store(uploadRecord);
+
+        return result;
+    }
+
+
+
 
     public String dataImport(final LoggedInConnection loggedInConnection, final  InputStream uploadFile, final boolean create) throws Exception {
         // OK I think I'm supposed to use language in here but how??? Will go to default name for the moment
@@ -161,8 +226,6 @@ public final class ImportService {
         return "";
     }
 
-    // I'm guessing from the internet?
-
 
 
     private String getFirstName(final String nameGiven) {
@@ -176,6 +239,151 @@ public final class ImportService {
         }
         return nameGiven.substring(0, commaPos);
     }
+    public String unzip(String fileName, String fileSuffix) {
+        String outputFile = "";
+        byte[] buffer = new byte[1024];
+        try {
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(fileName));
+            outputFile = fileName.substring(0, fileName.length() - 4);
+                //get the zip file content
+            //get the zipped file list entry - there should only be one
+            ZipEntry ze = zis.getNextEntry();
+
+            //while (ze != null) {
+                FileOutputStream fos = new FileOutputStream(outputFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer);
+                }
+                fos.close();
+                //ze = zis.getNextEntry();
+           // }
+            zis.closeEntry();
+            zis.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return outputFile;
+      }
+
+
+
+    private static byte[] codes = new byte[256];
+
+                 static {
+                 for (int i = 0; i < 256; i++) {
+                         codes[i] = -1;
+                     }
+                 for (int i = 'A'; i <= 'Z'; i++) {
+                         codes[i] = (byte) (i - 'A');
+                     }
+                 for (int i = 'a'; i <= 'z'; i++) {
+                         codes[i] = (byte) (26 + i - 'a');
+                     }
+                 for (int i = '0'; i <= '9'; i++) {
+                         codes[i] = (byte) (52 + i - '0');
+                     }
+                 codes['+'] = 62;
+                 codes['/'] = 63;
+             }
+
+
+
+    private static String decode64(final InputStream data, final String fileName) {
+
+         String tempName = "";
+         try{
+
+            String fileSuffix = fileName.substring(fileName.length() - 4);
+
+            File temp = File.createTempFile(fileName.substring(0, fileName.length() - 4), fileSuffix);
+            tempName = temp.getPath();
+
+            temp.deleteOnExit();
+            FileOutputStream fos = new FileOutputStream(tempName);
+            byte b[] = new byte[1];
+             int shift = 0;   // # of excess bits stored in accum
+             int accum = 0;   // excess bits
+             int count = data.read(b);
+             while (count > 0 ){
+                 if (codes[b[0]] >= 0){
+                     accum <<= 6;            // bits shift up by 6 each time thru
+                    shift += 6;             // loop, with new bits being put in
+
+                    accum |= codes[b[0]];         // at the bottom.
+                    if (shift >= 8) {       // whenever there are 8 or more shifted in,
+                        shift -= 8;         // write them out (from the top, leaving any
+                        fos.write((accum >> shift) & 0xff);
+                    }
+                }
+                count = data.read(b);
+                if (count<= 0){
+                    if (shift > 0){
+                        //not sure here....
+                        b[0] = ' ';
+                    }
+                }
+            }
+
+
+            //write it
+            fos.close();
+
+            System.out.println("Done");
+
+
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+         }
+        return tempName;
+     }
+
+    private void readExcel(final String fileName){
+
+
+        try {
+            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(fileName));
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            HSSFSheet sheet = wb.getSheetAt(0);
+            HSSFRow row;
+            HSSFCell cell;
+
+            int rows; // No of rows
+            rows = sheet.getPhysicalNumberOfRows();
+
+            int cols = 0; // No of columns
+            int tmp = 0;
+
+            // This trick ensures that we get the data properly even if it doesn't start from first few rows
+            for(int i = 0; i < 10 || i < rows; i++) {
+                row = sheet.getRow(i);
+                if(row != null) {
+                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+                    if(tmp > cols) cols = tmp;
+                }
+            }
+
+            for(int r = 0; r < rows; r++) {
+                row = sheet.getRow(r);
+                if(row != null) {
+                    for(int c = 0; c < cols; c++) {
+                        cell = row.getCell((short)c);
+                        if(cell != null) {
+                            // Your code here
+                        }
+                    }
+                }
+            }
+        } catch(Exception ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
+
 
 
 }
+
+
+
