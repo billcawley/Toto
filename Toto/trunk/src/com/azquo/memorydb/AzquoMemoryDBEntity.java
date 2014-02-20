e7bd705bf69db36bbfcf8c1514d861c7c1a7f8ea
Edd Cawley <edd@azquo.com>
2014-02-20 14:00:05 +0000

    comments
    git-svn-id: http://192.168.1.10/repos/toto@356 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/memorydb/AzquoMemoryDBEntity.java
+++ Toto/trunk/src/com/azquo/memorydb/AzquoMemoryDBEntity.java
@@ -10,25 +10,15 @@ import java.util.Set;
  * User: cawley
  * Date: 17/10/13
  * Time: 09:23
- * <p/>
- * After some thinking and learning about generics : entity objects should have as little reference to Mysql as possible.
- * Hence I'm going to move the row mapper, table name and column name value map out of here
-  * <p/>
+ * After some thinking and learning about generics : entity objects should have as little reference to Mysql as possible - the table they live in should be it?
  * OK with the new in memory DB thing these objects form the in memory database - it would be awkward to make them immutable
  * as I'd kind of like to so instead we want to make it so that it's very clear that modification after creation will
  * AUTOMATICALLY be reflected in MySQL as it catches up . . .
  * TODO : how to lock an object while it's being persisted??? Note time of modification?
- * <p/>
- * Also, these objects act as data objects hence each object SHOULD only exist in context of a azquo memory db
- * Writing the code to check this may be a pain and have some performance hits but the idea that changes to the
- * memory database are not checked by DB id worries me so much I'm going to try now.
- * <p/>
- * in summary : id and database id need to be rigidly controlled in this object or all hell could break loose
- * <p/>
+ * Also, these objects act as data objects hence each object SHOULD only exist in context of a azquo memory db, code here is designed to enforce this
+ * id and database id need to be rigidly controlled in this object or all hell could break loose
  * I think I may even go so far as actually holding the object reference to the database as opposed to a database id.
- * <p/>
  * If there is a way I don't yet know about for a constructor to only be called by another class this could be simplified
- * <p/>
  * So far have seen no performance hits from constraining objects by DB
  */
 
@@ -39,15 +29,14 @@ public abstract class AzquoMemoryDBEntity {
     // I am going to hold a reference here, then we simply compare objects by == to check that objects are in their created databases
 
     private final AzquoMemoryDB azquoMemoryDB;
-
+    // final id, can on;y be set in the constructor, we like that!
     private final int id;
     // it's a new object, this is private as all dealt with here
     private boolean needsInserting;
     // flag for deletion, to be picked up by the persistence
     protected boolean needsDeleting;
 
-    // I think protected is right here, while the class may be referenced externally (for generics) this makes it difficult to be subclassed externally?
-    //key with this is it makes the setting of an Id only in context of a memory db
+    //key with this is it makes the setting of an Id only in context of a memory db and hence one can only make one of these in this package (I think!)
     protected AzquoMemoryDBEntity(final AzquoMemoryDB azquoMemoryDB, final int id) throws Exception {
         this.azquoMemoryDB = azquoMemoryDB;
         // This getNeedsLoading is important, an instance of AzquoMemoryDB should only be in needsloading during the constructor and hence it will stop
@@ -71,19 +60,19 @@ public abstract class AzquoMemoryDBEntity {
         needsDeleting = false;
     }
 
-    // each class will say where it's persisted
+    // each class will say where it's persisted - this seems to be the most simple way to indicate shich persistence set to put this in
     protected abstract StandardDAO.PersistedTable getPersistTable();
 
     // all entities need this to save in the key/pair store
     public abstract String getAsJson();
 
-    // this seems to be the best way too constrain, others in teh package can get the instance but not reset it's value
+    // this seems to be the best way to constrain, others in teh package can get the instance but not reset it's value
+    // not that they could anyway, it's final
     protected final AzquoMemoryDB getAzquoMemoryDB() {
         return azquoMemoryDB;
     }
 
     // no setter for id, that should only be done by the constructor
-
     public final int getId() {
         return id;
     }
@@ -108,7 +97,7 @@ public abstract class AzquoMemoryDBEntity {
         return getId() == azquoMemoryDBEntity.getId();
     }
 
-    // should be called pretty much wwherever objects are going to be added to each others maps/sets/lists
+    // should be called pretty much wherever objects are going to be added to each others maps/sets/lists
 
     protected final void checkDatabaseMatches(final AzquoMemoryDB azquoMemoryDB) throws Exception {
         if (this.azquoMemoryDB != azquoMemoryDB) {
@@ -128,7 +117,7 @@ public abstract class AzquoMemoryDBEntity {
 
     protected final void setAsPersisted() {
         needsInserting = false;
-        azquoMemoryDB.removeEntityNeedsPersisting(getPersistTable(),this);
+        azquoMemoryDB.removeEntityNeedsPersisting(getPersistTable(), this);
     }
 
     protected final void setNeedsPersisting() {
