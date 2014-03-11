6197aba3aef35a023c66281173a2e27cc6bae68c
Bill Cawley <bill@azquo.com>
2014-03-11 15:30:30 +0000

    Used to parse JSON for Value calls
    git-svn-id: http://192.168.1.10/repos/toto@394 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/src/com/azquo/jsonrequestentities/ValueJsonRequest.java
@@ -0,0 +1,26 @@
+package com.azquo.jsonrequestentities;
+
+import java.util.Map;
+
+/**
+ * Created by bill on 03/03/14.
+ */
+public class ValueJsonRequest {
+
+
+         public String rowheadings;
+         public String columnheadings;
+         public String context;
+         public String connectionid;
+         public String region;
+         public String lockmap;
+         public String editeddata;
+         public String searchbynames;
+         public String jsonfunction;
+         public String user;
+         public String password;
+         public String spreadsheetname;
+         public String database;
+
+
+}
