93517e8bbfe97a5b8cd4fd00c35fe6749acea509
Bill Cawley <bill@azquo.com>
2014-03-11 15:16:07 +0000

    To handle Login requests as JSON
    git-svn-id: http://192.168.1.10/repos/toto@385 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/src/com/azquo/jsonrequestentities/LoginJsonRequest.java
@@ -0,0 +1,18 @@
+package com.azquo.jsonrequestentities;
+
+import java.util.Map;
+
+/**
+ * Created by bill on 03/03/14.
+ */
+public class LoginJsonRequest {
+
+      public String database = "";
+      public String user = "";
+      public String password = "";
+      public String spreadsheetname = "";
+      public String timeout = "";
+      public String connectionid = "";
+
+
+}
