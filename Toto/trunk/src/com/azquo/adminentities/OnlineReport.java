4e843c933cedb30837cc49e35d5cbdb4a05bb1d6
Bill Cawley <bill@azquo.com>
2014-04-16 17:35:31 +0000

    New table to store online report info
    git-svn-id: http://192.168.1.10/repos/toto@523 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/src/com/azquo/adminentities/OnlineReport.java
@@ -0,0 +1,97 @@
+package com.azquo.adminentities;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.xml.internal.bind.api.impl.NameConverter;
+
+import java.util.Date;
+
+/**
+ * Created by bill on 15/04/14.
+ */
+public class OnlineReport extends StandardEntity {
+
+    public static final String STATUS_ADMINISTRATOR = "ADMINISTRATOR";
+
+    private int businessId;
+    private int databaseId;
+    private String database; // for sending parameters only
+    private String reportName;
+    private String userStatus;
+    private String json;
+
+     @JsonCreator
+    public OnlineReport(@JsonProperty("id") int id
+            , @JsonProperty("businessId") int businessId
+            , @JsonProperty("databaseId") int databaseId
+             , @JsonProperty("database") String database
+            , @JsonProperty("reportName") String reportName
+            , @JsonProperty("userStatus") String userStatus
+            , @JsonProperty("json") String json) {
+        this.id = id;
+        this.businessId = businessId;
+        this.databaseId = databaseId;
+        this.database = database;
+        this.reportName = reportName;
+        this.userStatus = userStatus;
+        this.json = json;
+    }
+
+    public int getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(int businessId) {
+        this.businessId = businessId;
+    }
+
+    public int getDatabaseId() {
+        return databaseId;
+    }
+
+    public void setDatabaseId(int databaseId) { this.databaseId = databaseId; }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getReportName() {
+        return reportName;
+    }
+
+    public void setReportName(String reportName) {
+        this.reportName = reportName;
+    }
+
+    public String getUserStatus() {return userStatus;  }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "id=" + id +
+                ", businessId=" + businessId +
+                ", databaseId=" + databaseId +
+                ", reportName='" + reportName +'\'' +
+                ", userStatus='" + userStatus + '\'' +
+                ", json='" + json + '\'' +
+                '}';
+    }
+}
