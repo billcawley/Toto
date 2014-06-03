d3f853044e5e3b5eb05b531caf0a46149d8fc9ee
Bill Cawley <bill@azquo.com>
2014-06-03 09:51:46 +0000

    This is now used as the main URL for online admin.
    git-svn-id: http://192.168.1.10/repos/toto@588 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/controller/LoginController.java
+++ Toto/trunk/src/com/azquo/controller/LoginController.java
@@ -1,20 +1,33 @@
 package com.azquo.controller;
 
+import com.azquo.admindao.OnlineReportDAO;
+import com.azquo.adminentities.OnlineReport;
 import com.azquo.jsonrequestentities.LoginJsonRequest;
 import com.azquo.service.LoggedInConnection;
 import com.azquo.service.LoginService;
+import com.azquo.service.OnlineService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.InputStream;
 import java.net.URLDecoder;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created with IntelliJ IDEA.
@@ -33,9 +46,17 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    private OnlineService onlineService;
+
+    @Autowired
+    private OnlineReportDAO onlineReportDAO;
+
     @RequestMapping
     @ResponseBody
     public String handleRequest(HttpServletRequest request)throws Exception{
+
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 
         Enumeration<String> parameterNames = request.getParameterNames();
 
@@ -47,6 +68,11 @@ public class LoginController {
         String json=null;
         String database=null;
         String checkConnectionId = null;
+        String result = null;
+        String rowChosen = "";
+        String colChosen = "";
+        String jsonFunction = "azquojsonfeed";
+        boolean online = false;
         while (parameterNames.hasMoreElements()) {
             String paramName = parameterNames.nextElement();
             String paramValue = request.getParameterValues(paramName)[0];
@@ -66,8 +92,105 @@ public class LoginController {
                 database = paramValue;
             }else if (paramName.equals("checkconnectionid")){
                 checkConnectionId = paramValue;
+            }else if (paramName.equals("online")){
+                online = true;
+            }else if (paramName.equals("rowchosen")){
+                rowChosen = paramValue;
+            }else if (paramName.equals("colchosen")){
+                colChosen = paramValue;
+            }else if (paramName.equals("jsonfunction")){
+                jsonFunction = paramValue;
             }
+
         }
+        FileItem item = null;
+        FileItem file = null;
+        if (isMultipart){
+            // Create a factory for disk-based file items
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+
+// Set factory constraints
+            factory.setSizeThreshold(10000000);
+            ServletContext servletContext = request.getServletContext();
+            File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+            factory.setRepository(repository);
+
+// Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+// Set overall request size constraint
+            upload.setSizeMax(100000000);
+
+// Parse the request
+            List<FileItem> items = upload.parseRequest(request);
+            Iterator it = items.iterator();
+            item = (FileItem) it.next();
+            boolean macMode = false;
+            if (!item.getFieldName().equals("parameters")) { // no parameters file passed, used to be a plain error but mac may have other ideas - parameters sent via curl
+                while (item != null){
+                    if (item.getName() !=null){
+                        file = item;
+                    }else {
+                        if (item.getFieldName().equals("connectionid")) {
+                            macMode = true;
+                            connectionId = item.getString();
+                        } else if (item.getFieldName().equals("spreadsheetname")) {
+                            spreadsheetName = item.getString();
+                        } else if (item.getFieldName().equals("rowchosen")) {
+                            rowChosen = item.getString();
+                        } else if (item.getFieldName().equals("colchosen")) {
+                            colChosen = item.getString();
+                        } else if (item.getFieldName().equals("database")) {
+                            database = item.getString();
+                        }
+                    }
+                    if (it.hasNext()) {
+                        item = (FileItem) it.next();
+                    }else{
+                        item = null;
+                    }
+                }
+
+                if (!macMode){ // either mac or windows not sending what we want
+                    return "error: expecting parameters";
+                }
+            } else { // parameters file built on windows
+                String parameters = item.getString();
+                StringTokenizer st = new StringTokenizer(parameters, "&");
+                while (st.hasMoreTokens()) {
+                    file = item;
+                    String parameter = st.nextToken();
+                    if (!parameter.endsWith("=")){
+                        StringTokenizer st2 = new StringTokenizer(parameter, "=");
+                        String parameterName = st2.nextToken();
+                        if (parameterName.equals("connectionid")) {
+                            connectionId = st2.nextToken();
+                        }
+                        if (parameterName.equals("spreadsheetname")) {
+                            spreadsheetName = st2.nextToken();
+                        }
+                        if (parameterName.equals("rowchosen")) {
+                            rowChosen = st2.nextToken();
+                        }
+                        if (parameterName.equals("colchosen")) {
+                            colChosen = st2.nextToken();
+                        }
+                        if (parameterName.equals("database")){
+                            database = st2.nextToken();
+                        }
+                    }
+                }
+                if (it.hasNext()){
+                    item = (FileItem) it.next();
+                }else{
+                    item = null;
+
+                }
+            }
+
+        }
+        //assuming that the last item is the file;
+        item = file;
         String callerId = request.getRemoteAddr();
         if (callerId != null && userEmail != null && userEmail.equals("demo@user.com")){
             userEmail += callerId;
@@ -92,10 +215,11 @@ public class LoginController {
 
         }
 
-        if (connectionId != null && connectionId.length() > 0 && loginService.getConnection(connectionId) != null) {
+        if (!online && connectionId != null && connectionId.length() > 0 && loginService.getConnection(connectionId) != null) {
                 return connectionId;
         }
-        if (userEmail != null && userEmail.length() > 0 && password != null && password.length() > 0) {
+        LoggedInConnection loggedInConnection = null;
+        if ((connectionId == null || connectionId.equals("aborted")) && userEmail != null && userEmail.length() > 0 && password != null && password.length() > 0) {
             logger.info("spreadsheet name " + spreadsheetName);
             if (database == null || database.length() == 0) {
                 database = "unknown";
@@ -109,17 +233,29 @@ public class LoginController {
                 }
             }
 
-            final LoggedInConnection loggedInConnection = loginService.login(database, userEmail, password, minutesTimeout, spreadsheetName, false);
-            if (loggedInConnection != null) {
-                return loggedInConnection.getConnectionId();
-            }
+           loggedInConnection = loginService.login(database, userEmail, password, minutesTimeout, spreadsheetName, false);
+        }else {
+            loggedInConnection = loginService.getConnection(connectionId);
         }
+         if (loggedInConnection != null) {
+                result =  loggedInConnection.getConnectionId();
+                if (online){
+                    String message = "";
+                    if (rowChosen.length() > 0){
+                        message = onlineService.followInstructionsAt(loggedInConnection, jsonFunction, Integer.parseInt(rowChosen), Integer.parseInt(colChosen), database, item);
+                    }
+                        OnlineReport onlineReport = onlineReportDAO.findById(1);//TODO  Sort out where the maintenance sheet should be referenced
+                        return onlineService.readExcel(loggedInConnection, onlineReport, spreadsheetName, message);
+                  }
+                return result;
+            }
         if (checkConnectionId != null && checkConnectionId.length() > 0) {
             if (loginService.getConnection(checkConnectionId) != null) {
-                return "ok";
+                result = "ok";
             } else {
-                return "error:expired or incorrect connection id";
+                result =  "error:expired or incorrect connection id";
             }
+            return result;
         }
         return "error:incorrect login details";
     }
