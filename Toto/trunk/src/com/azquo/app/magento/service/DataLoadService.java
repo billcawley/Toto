0fb2ff7a1b4dd7285a2d74943ed73ffdbb0ba899
Edd Cawley <edd@azquo.com>
2014-08-11 20:27:37 +0000

    basic structure
    git-svn-id: http://192.168.1.10/repos/toto@672 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/app/magento/service/DataLoadService.java
+++ Toto/trunk/src/com/azquo/app/magento/service/DataLoadService.java
@@ -1,6 +1,8 @@
 package com.azquo.app.magento.service;
 
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -9,9 +11,15 @@ import java.util.*;
 public final class DataLoadService {
 
     private final Map<String, List<Map<String, String>>> tableMap;
+    final Map<Integer, MagentoProduct> products;
+    final Map<Integer, MagentoCategory> categories;
+    final Map<Integer, MagentoOrderLineItem> orderLineItems;
 
     public DataLoadService() throws IOException {
         tableMap = new HashMap<String, List<Map<String, String>>>();
+        products = new HashMap<Integer, MagentoProduct>();
+        categories = new HashMap<Integer, MagentoCategory>();
+        orderLineItems = new HashMap<Integer, MagentoOrderLineItem>();
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("/home/cawley/magentodatadump.txt")));
 
         String line;
@@ -66,7 +74,6 @@ public final class DataLoadService {
             }
         }
 
-        ArrayList<MagentoProduct> products = new ArrayList<MagentoProduct>();
         if (productEntityTypeRecord != null){
             for (Map<String, String> entityTypeRecord : tableMap.get("catalog_product_entity")){
                 MagentoProduct magentoProduct = new MagentoProduct();
@@ -87,16 +94,232 @@ public final class DataLoadService {
                 }
                 magentoProduct.attributes = productAttributesAndValues;
                 System.out.println("Adding product : " + magentoProduct.attributes.get("Name") + " atts : " + magentoProduct.attributes);
-                products.add(magentoProduct);
+                products.put(magentoProduct.id, magentoProduct);
             }
         }
-        System.out.println("attempted to organise some product data");
+
+        Map<String, String> categoryEntityTypeRecord = null;
+        for (Map<String, String> entityTypeRecord : tableMap.get("eav_entity_type")){
+            if (entityTypeRecord.get("entity_type_code").equals("catalog_category")){
+                categoryEntityTypeRecord = entityTypeRecord;
+            }
+        }
+
+        List<Map<String, String>> categoryAttributes = new ArrayList<Map<String, String>>();
+        for (Map<String, String> attribute : tableMap.get("eav_attribute")) {
+            if (attribute.get("entity_type_id").equals(categoryEntityTypeRecord.get("entity_type_id"))) { // ok we're going to get a list of relevant attributes for the product then . . .
+                categoryAttributes.add(attribute);
+            }
+        }
+
+        if (categoryEntityTypeRecord != null){
+            for (Map<String, String> entityTypeRecord : tableMap.get("catalog_category_entity")){
+                MagentoCategory magentoCategory = new MagentoCategory();
+                magentoCategory.id = Integer.parseInt(entityTypeRecord.get("entity_id"));
+                if (entityTypeRecord.get("parent_id") != null){
+                    magentoCategory.parent_id = Integer.parseInt(entityTypeRecord.get("parent_id"));
+                } else {
+                    magentoCategory.parent_id = 0;
+                }
+                magentoCategory.path = entityTypeRecord.get("path");
+                if (entityTypeRecord.get("position") != null){
+                    magentoCategory.position = Integer.parseInt(entityTypeRecord.get("position"));
+                } else {
+                    magentoCategory.position = 0;
+                }
+                if (entityTypeRecord.get("level") != null){
+                    magentoCategory.level = Integer.parseInt(entityTypeRecord.get("level"));
+                } else {
+                    magentoCategory.level = 0;
+                }
+                Map<String, String> categoryAttributesAndValues = new HashMap<String, String>();
+                for (Map<String, String> attribute : categoryAttributes){
+                    String attributeName = attribute.get("frontend_label");
+                    // now see if there's a value for this thing???
+                    if (!attribute.get("backend_type").equals("static")){ // static means the value is in teh entity table
+                        for (Map<String, String> possibleValueRow : tableMap.get("catalog_category_entity" + "_" + attribute.get("backend_type"))){ // should (!) have us looking in teh right place
+                            if (possibleValueRow.get("attribute_id").equals(attribute.get("attribute_id")) && possibleValueRow.get("entity_id").equals(entityTypeRecord.get("entity_id"))){ // then it should (!) be the value we're looking for?
+                                categoryAttributesAndValues.put(attributeName, possibleValueRow.get("value"));
+                            }
+                        }
+                    }
+                }
+                magentoCategory.attributes = categoryAttributesAndValues;
+                System.out.println("Adding category : " + magentoCategory.attributes.get("Name") + " atts : " + magentoCategory.attributes);
+                categories.put(magentoCategory.id, magentoCategory);
+            }
+        }
+
+/*        int id;
+        int parent_id;
+        Date created;
+        String name;
+        String sku;
+        double quantityOrdered;
+        double basePrice;
+        double rowTotalIncludingTax;*/
+
+        //2013-04-04 01:23:18
+
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        for (Map<String, String> orderItem : tableMap.get("sales_flat_order_item")){
+            MagentoOrderLineItem magentoOrderLineItem = new MagentoOrderLineItem();
+            magentoOrderLineItem.id = Integer.parseInt(orderItem.get("item_id"));
+            if (orderItem.get("parent_item_id") != null){
+                magentoOrderLineItem.parent_id = Integer.parseInt(orderItem.get("parent_item_id"));
+            } else {
+                magentoOrderLineItem.parent_id = 0;
+            }
+            magentoOrderLineItem.product_id = Integer.parseInt(orderItem.get("product_id"));
+            try {
+                magentoOrderLineItem.created = inputDateFormat.parse(orderItem.get("created_at"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            magentoOrderLineItem.name = orderItem.get("name");
+            magentoOrderLineItem.sku = orderItem.get("sku");
+            magentoOrderLineItem.quantityOrdered = Double.parseDouble(orderItem.get("qty_ordered"));
+            magentoOrderLineItem.basePrice = Double.parseDouble(orderItem.get("base_price"));
+            if (orderItem.get("row_total_incl_tax") != null){
+                magentoOrderLineItem.rowTotalIncludingTax = Double.parseDouble(orderItem.get("row_total_incl_tax"));
+            } else {
+                magentoOrderLineItem.rowTotalIncludingTax = 0;
+            }
+            orderLineItems.put(magentoOrderLineItem.id, magentoOrderLineItem);
+        }
+
+        System.out.println("load complete");
+        System.out.println(getMagentoStructure());
+
     }
 
     public class MagentoProduct {
         public int id;
         public String sku;
         public Map<String, String> attributes;
+        public Set<MagentoProduct> getChildren(){
+            Set<MagentoProduct> toReturn = new HashSet<MagentoProduct>();
+            for (Map<String, String> product_relation : tableMap.get("catalog_product_relation")){
+                if (product_relation.get("parent_id").equals(id + "")){
+                    toReturn.add(products.get(Integer.parseInt(product_relation.get("child_id"))));
+                }
+            }
+            return toReturn;
+        }
+        public Set<MagentoOrderLineItem> getOrderLines(){
+            Set<MagentoOrderLineItem> toReturn = new HashSet<MagentoOrderLineItem>();
+            for (MagentoOrderLineItem orderLineItem : orderLineItems.values()){
+                if (orderLineItem.product_id == id){
+                    toReturn.add(orderLineItem);
+                }
+            }
+            return toReturn;
+        }
+    }
+
+    public class MagentoCategory {
+        public int id;
+        public int parent_id;
+        public String path;
+        public int position;
+        public int level;
+        public Map<String, String> attributes;
+        public Set<MagentoCategory> getChildren(){
+            Set<MagentoCategory> toReturn = new HashSet<MagentoCategory>();
+            for (MagentoCategory category : categories.values()){
+                if (category.parent_id == id){
+                    toReturn.add(category);
+                }
+            }
+            return toReturn;
+        }
+        public Set<MagentoProduct> getProducts(){
+            Set<MagentoProduct> toReturn = new HashSet<MagentoProduct>();
+            for (Map<String, String> category_product_link : tableMap.get("catalog_category_product")){
+                if (category_product_link.get("category_id").equals(id + "")){
+                    toReturn.add(products.get(Integer.parseInt(category_product_link.get("product_id"))));
+                }
+            }
+            return toReturn;
+        }
+    }
+    // just the info we need for the moment, ignoring most columns
+    public class MagentoOrderLineItem {
+        int id;
+        int parent_id;
+        int product_id;
+        Date created;
+        String name;
+        String sku;
+        double quantityOrdered;
+        double basePrice;
+        double rowTotalIncludingTax;
+
+        @Override
+        public String toString() {
+            return "MagentoOrderLineItem{" +
+                    "id=" + id +
+                    ", parent_id=" + parent_id +
+                    ", product_id=" + product_id +
+                    ", created=" + created +
+                    ", name='" + name + '\'' +
+                    ", sku='" + sku + '\'' +
+                    ", quantityOrdered=" + quantityOrdered +
+                    ", basePrice=" + basePrice +
+                    ", rowTotalIncludingTax=" + rowTotalIncludingTax +
+                    '}';
+        }
+    }
+
+    public String getMagentoStructure(){
+        StringBuilder toReturn = new StringBuilder();
+        for (MagentoCategory category : categories.values()){
+            if (category.parent_id == 0){
+                toReturn.append(getCategoryStructure(category, 0));
+            }
+        }
+        return toReturn.toString();
+    }
+
+    public String getCategoryStructure(MagentoCategory category, int tab){
+        String toReturn = new String();
+        for (int i = 0; i <= tab; i++){
+            toReturn += "\t";
+        }
+        toReturn += "Category : " + category.attributes.get("Name") + "\n";
+        Set<MagentoCategory> children = category.getChildren();
+        for (MagentoCategory category1 : children){
+            toReturn += getCategoryStructure(category1, tab + 1);
+        }
+        Set<MagentoProduct> products = category.getProducts();
+        for (MagentoProduct product : products){
+            toReturn += getProductStructure(product, tab + 1);
+        }
+
+        return toReturn.toString();
+    }
+
+    public String getProductStructure(MagentoProduct product, int tab){
+        String toReturn = new String();
+        for (int i = 0; i <= tab; i++){
+            toReturn += "\t";
+        }
+        toReturn += "Product : " + product.attributes.get("Name") + "\n";
+        Set<MagentoProduct> children = product.getChildren();
+        for (MagentoProduct product1 : children){
+            toReturn += getProductStructure(product1, tab + 1);
+        }
+        for (MagentoOrderLineItem orderLineItem : product.getOrderLines()){
+            for (int i = 0; i <= tab; i++){
+                toReturn += "\t";
+            }
+            toReturn += "\tOrder line  : " + orderLineItem.toString() + "\n";
+        }
+
+
+        return toReturn.toString();
     }
 
 }
