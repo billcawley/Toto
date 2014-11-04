d26f7bfdc182e04c30f0b21253e94c111d4cfed5
Bill Cawley <bill@azquo.com>
2014-11-04 09:53:23 +0000

    For Magento
    git-svn-id: http://192.168.1.10/repos/toto@805 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/src/com/azquo/app/magento/controller/MagentoController.java
@@ -0,0 +1,101 @@
+package com.azquo.app.magento.controller;
+
+import com.azquo.admindao.OnlineReportDAO;
+import com.azquo.adminentities.OnlineReport;
+import com.azquo.app.magento.service.DataLoadService;
+import com.azquo.service.LoggedInConnection;
+import com.azquo.service.LoginService;
+import com.azquo.service.OnlineService;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created by bill on 28/10/14.
+ */
+
+@Controller
+@RequestMapping("/Magento")
+
+
+public class MagentoController {
+
+    @Autowired
+    private DataLoadService dataLoadService;
+
+    @Autowired
+    OnlineReportDAO onlineReportDAO;
+
+    @Autowired
+    OnlineService onlineService;
+
+    @Autowired
+    LoginService loginService;
+
+    private static final Logger logger = Logger.getLogger(MagentoController.class);
+
+
+    @RequestMapping
+    @ResponseBody
+    public String handleRequest(HttpServletRequest request) throws Exception {
+     LoggedInConnection loggedInConnection = null;
+     try {
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+// Configure a repository (to ensure a secure temp location is used)
+        ServletContext servletContext = request.getServletContext();
+        File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+        factory.setRepository(repository);
+
+// Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+// Parse the request
+        String connectionId = request.getParameter("connectionid");
+        String data = request.getParameter("data");
+        String op = request.getParameter("op");
+         if (op==null) op = "";
+         if (connectionId != null){
+             loggedInConnection = loginService.getConnection(connectionId);
+         }
+         if (loggedInConnection == null) {
+             //for testing only
+             loggedInConnection = loginService.login("temporary","tempuser", "password",0,"",false);
+        }
+        if (op.equals("tempdb")) {
+            dataLoadService.loadData(loggedInConnection, data);
+             return loggedInConnection.getConnectionId() + "";
+            //return onlineService.readExcel(loggedInConnection, onlineReport, null, "");
+        }
+         if (op.equals("reports")){
+             OnlineReport onlineReport = onlineReportDAO.findById(1);//TODO  Sort out where the maintenance sheet should be referenced
+             return onlineService.readExcel(loggedInConnection, onlineReport, null, "");
+
+         }
+         return "unknown op";
+     } catch (Exception e) {
+        e.printStackTrace();
+        return "error:" + e.getMessage();
+    }
+}
+
+
+}
+
+
+
