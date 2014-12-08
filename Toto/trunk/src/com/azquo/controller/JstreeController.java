5f5077723c79fb31b7822f8d96b4f97395a84506
Bill Cawley <bill@azquo.com>
2014-12-08 17:12:29 +0000

    getting closer to finished....
    git-svn-id: http://192.168.1.10/repos/toto@867 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/controller/JstreeController.java
+++ Toto/trunk/src/com/azquo/controller/JstreeController.java
@@ -4,9 +4,11 @@ import com.azquo.admindao.DatabaseDAO;
 import com.azquo.admindao.OnlineReportDAO;
 import com.azquo.adminentities.Database;
 import com.azquo.adminentities.OnlineReport;
+import com.azquo.jsonrequestentities.NameJsonRequest;
 import com.azquo.memorydb.Name;
 import com.azquo.service.*;
 //import com.azquo.util.Chart;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -37,7 +39,13 @@ public class JstreeController {
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    OnlineService onlineService;
+
     private static final Logger logger = Logger.getLogger(JstreeController.class);
+
+    private static final ObjectMapper jacksonMapper = new ObjectMapper();
+
 
     @RequestMapping
     public String handleRequest (ModelMap model,HttpServletRequest request, HttpServletResponse response)throws Exception{
@@ -52,100 +60,137 @@ public class JstreeController {
         String position = request.getParameter("position");
         String parent = request.getParameter("parent");
         LoggedInConnection loggedInConnection = null;
+        String json = request.getParameter("json");
+        String parents = request.getParameter("parents");
+        String topNode = request.getParameter("topnode");//only for use at root.
         String jsonFunction = "azquojsonfeed";
-        if (connectionId == null) {
-            if (user.equals("demo@user.com")){
-                user += request.getRemoteAddr();
-            }
-            loggedInConnection = loginService.login(database, user, password, 0, "", false);
-
-            if (loggedInConnection == null) {
-                model.addAttribute("content", "error:no connection id");
-                return "utf8page";
-            }
-            connectionId = loggedInConnection.getConnectionId();
-
-        }
-        loggedInConnection = loginService.getConnection(connectionId);
-        try {
+         try {
             StringBuffer result = new StringBuffer();
-            Map<String, Integer> lookup = loggedInConnection.getJsTreeIds();
-            Integer id = 0;
-            if (jsTreeId.equals("#")){
-                jsTreeId = "0";
-            } else{
-                id = lookup.get(jsTreeId);
-            }
-            if (jsTreeId.equals("true")){
-                id = lookup.get(parent);
-            }
-            Name name = null;
-            if (id==null&& op.equals("rename_node")){
-                //a new node has just been created
-            }
-            if (id > 0){
-                name = nameService.findById(loggedInConnection,id);
-                if (name == null){
-                    result.append("error: unknown node");
-                }
-            }
-            if (result.length()== 0) {
-                if (op.equals("create_node")){
-                    Name newName = nameService.findOrCreateNameInParent(loggedInConnection,"newnewnew",name,true);
-                    newName.setAttributeWillBePersisted(Name.DEFAULT_DISPLAY_NAME,"New node");
-                    result.append("true");
-                }
+             if (connectionId == null) {
+                 if (user.equals("demo@user.com")){
+                     user += request.getRemoteAddr();
+                 }
+                 loggedInConnection = loginService.login(database, user, password, 0, "", false);
 
-                if (op.equals("rename_node")){
-                    name.setAttributeWillBePersisted(Name.DEFAULT_DISPLAY_NAME,position);
-                    result.append("true");
-                }
-                if (op.equals("children")) {
-                    jsonFunction = null;
-                    result.append("[{\"id\":" + jsTreeId + ",\"text\":\"");
-                    List<Name> children = new ArrayList<Name>();
-                    if (jsTreeId.equals("0")) {
-                        loggedInConnection.setLastJstreeId(0);
-                        lookup = new HashMap<String, Integer>();
-                        loggedInConnection.setJsTreeIds(lookup);
-                        result.append("root");
-                        children = nameService.findTopNames(loggedInConnection);
+                 if (loggedInConnection == null) {
+                     model.addAttribute("content", "error:no connection id");
+                     return "utf8page";
+                 }
+                 connectionId = loggedInConnection.getConnectionId();
 
-                    } else {
-                        Name nodeName = nameService.findById(loggedInConnection, id);
-                        result.append(nodeName.getDefaultDisplayName());
-                        for (Name child : name.getChildren()) {
-                            children.add(child);
-                        }
+             }
+             loggedInConnection = loginService.getConnection(connectionId);
+             Map<String, LoggedInConnection.JsTreeNode> lookup = loggedInConnection.getJsTreeIds();
+             if (json != null && json.length() > 0) {
+                 NameJsonRequest nameJsonRequest;
+                try {
+                    nameJsonRequest = jacksonMapper.readValue(json, NameJsonRequest.class);
+                } catch (Exception e) {
+                    logger.error("name json parse problem", e);
+                    return "error:badly formed json " + e.getMessage();
+                }
+                if (loggedInConnection == null) {
+                    return "error:invalid connection id or login credentials";
+                }
+                LoggedInConnection.JsTreeNode currentNode = lookup.get(nameJsonRequest.id+ "");
+                 nameJsonRequest.id = currentNode.child.getId();//convert from jstree id.
+                 result.append(nameService.processJsonRequest(loggedInConnection, nameJsonRequest, loggedInConnection.getLanguages()));
+             }else{
+                 LoggedInConnection.JsTreeNode current = new LoggedInConnection.JsTreeNode(null,null);
+                 if (jsTreeId==null || jsTreeId.equals("#")){
+                     if (topNode != null){
+                         current.child = nameService.findById(loggedInConnection,Integer.parseInt(topNode));
+                     }
+                     jsTreeId = "0";
+                 } else{
+                     current = lookup.get(jsTreeId);
+                 }
+                 if (jsTreeId.equals("true")){
+                     current = lookup.get(parent);
+                 }
+                 if (current==null&& op.equals("rename_node")){
+                     //a new node has just been created
+                }
+                 if (op.equals("new")){
+                     if (parents==null) parents = "false";
+                     int rootId = 0;
+                     if (current.child != null){
+                         rootId = current.child.getId();
+                     }
+                     result.append(onlineService.showNameDetails(loggedInConnection, database, rootId,parents));
+                     model.addAttribute("content", result);
+                     return "utf8page";
+
+                 }
+                 if (op.equals("children")) {
+                     jsonFunction = null;
+                     result.append("[{\"id\":" + jsTreeId + ",\"state\":{\"opened\":true},\"text\":\"");
+                     List<Name> children = new ArrayList<Name>();
+                     if (jsTreeId.equals("0")&& current.child == null) {
+                         result.append("root");
+                         children = nameService.findTopNames(loggedInConnection);
+
+                     } else if (current.child != null) {
+                         result.append(current.child.getDefaultDisplayName());
+                         if (!parents.equals("true")) {
+                             for (Name child : current.child.getChildren()) {
+                                 children.add(child);
+                             }
+                         }else {
+                             for (Name nameParent : current.child.getParents()) {
+                                 children.add(nameParent);
+                             }
+                         }
+
+                     }
+
+                     result.append("\"");
+                     if (children.size() > 0) {
+                         result.append(",\"children\":[");
+                         int lastId = loggedInConnection.getLastJstreeId();
+                         int count = 0;
+                         for (Name child : children) {
+                             if (count++ > 0) {
+                                 result.append(",");
+                             }
+                             loggedInConnection.setLastJstreeId(++lastId);
+                             LoggedInConnection.JsTreeNode newNode = new LoggedInConnection.JsTreeNode(child, current.child);
+                             lookup.put(lastId + "", newNode);
+                             result.append("{\"id\":" + lastId + ",\"text\":\"" + child.getDefaultDisplayName() + "\"");
+                             if (child.getChildren().size() > 0) {
+                                 result.append(",\"children\":true");
+                             }
+                             result.append("}");
+
+                         }
+                         result.append("]");
+                     }
+                     result.append("}]");
+
+                 }
+                 if (current.child != null){
+                     if (op.equals("move_node")){
+                        lookup.get(parent).child.addChildWillBePersisted(current.child);
+                        result.append("true");
+                    }
+                    if (op.equals("create_node")) {
+                        Name newName = nameService.findOrCreateNameInParent(loggedInConnection, "newnewnew", current.child, true);
+                        newName.setAttributeWillBePersisted(Name.DEFAULT_DISPLAY_NAME, "New node");
+                        result.append("true");
+                    }
+
+                    if (op.equals("rename_node")) {
+                        current.child.setAttributeWillBePersisted(Name.DEFAULT_DISPLAY_NAME, position);
+                        result.append("true");
+                    }
+                    if (op.equals("details")) {
+                        result.append("true,\"namedetails\":" + nameService.jsonNameDetails(current.child));
+                        //result = jsonFunction + "({\"namedetails\":" + result + "})";
 
                     }
-                    result.append("\"");
-                    if (children.size() > 0) {
-                        result.append(",\"children\":[");
-                        int lastId = loggedInConnection.getLastJstreeId();
-                        int count = 0;
-                        for (Name child : children) {
-                            if (count++ > 0) {
-                                result.append(",");
-                            }
-                            loggedInConnection.setLastJstreeId(++lastId);
-                            lookup.put(lastId + "", child.getId());
-                            result.append("{\"id\":" + lastId + ",\"text\":\"" + child.getDefaultDisplayName() + "\"");
-                            if (child.getChildren().size() > 0) {
-                                result.append(",\"children\":true");
-                            }
-                            result.append("}");
-
-                        }
-                        result.append("]");
+                    if (result.length()==0){
+                        result.append("error:" + op + " not understood");
                     }
-                    result.append("}]");
-
-                }
-                if (op.equals("details")){
-                    result.append("true,\"namedetails\":" + nameService.jsonNameDetails(loggedInConnection, id));
-                    //result = jsonFunction + "({\"namedetails\":" + result + "})";
-
                 }
             }
             if (jsonFunction != null){
