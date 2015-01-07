be981f261c60d7890423a6fbca9fbee3a2bff95b
Bill Cawley <bill@azquo.com>
2015-01-07 13:19:04 +0000

    can save data
    git-svn-id: http://192.168.1.10/repos/toto@901 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/app/reviews/controller/MerchantAdminController.java
+++ Toto/trunk/src/com/azquo/app/reviews/controller/MerchantAdminController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,25 +39,32 @@ public class MerchantAdminController {
     public String handleRequest(ModelMap model,HttpServletRequest request, HttpServletResponse response) throws Exception {
         Map<String, String[]> parameterMap = request.getParameterMap();
 
+        System.out.println("Merchant admin accessed");
+        String testitem = request.getParameter("testitem");
+        if (testitem != null){
+            model.addAttribute("content", "received testitem = " + testitem);
+            return "utf8page";
+
+        }
+        List<String> values = new ArrayList<String>();
         String op = request.getParameter("op");
         String itemName = request.getParameter("itemname").replace("_"," ");
         String type = request.getParameter("itemtype");
         String nameId = request.getParameter("nameid");
         Map<String,String> fieldnames = new HashMap<String, String>();
+        String download = request.getParameter("download");
+        String field = request.getParameter("field");
         Map<String,String> fieldvalues = new HashMap<String, String>();
         String velocityTemplate = request.getParameter("velocitytemplate");
-
-        // scan through for parameters rating1 rating2 comment1 comment2 etc. One could scan with a for loop but this seems as good a way as any.
-        for (String paramName : parameterMap.keySet()){
-            String paramValue = parameterMap.get(paramName)[0];
-            if (paramName.startsWith("fieldname")){
-                String fieldno = paramName.substring(9);
-                fieldnames.put(fieldno,paramValue);
-            } else if (paramName.startsWith("fieldvalue")){
-                String comment = paramName.substring(10);
-                fieldvalues.put(comment, paramValue);
-            }
+        String submit = request.getParameter("submit");
+         // scan through for parameters rating1 rating2 comment1 comment2 etc. One could scan with a for loop but this seems as good a way as any.
+        int i=1;
+        String fieldVal = request.getParameter("field" + i++);
+        while (fieldVal != null){
+            values.add(fieldVal);
+            fieldVal = request.getParameter("field" + i++);
         }
+
 
         if (op==null){
             op="displaypage";
@@ -63,8 +72,16 @@ public class MerchantAdminController {
             if (type==null) type = "";
         }
         String result = "";
+        if (download!=null && download.length() > 0){
+            op = "";
+            reviewService.download(response, itemName, Integer.parseInt(nameId), field);
+        }
         if(op.equals("displaypage")){
-            result = reviewService.createPageSpec(itemName,type, nameId);
+            if (submit == null){
+                result = reviewService.createPageSpec(itemName,type, nameId);
+            }else{
+                reviewService.saveData(response, itemName,values, Integer.parseInt(nameId));
+            }
 
         }
 
