28eaa3acb2feaa7fe2b0a93c3f20e148a88de774
Bill Cawley <bill@azquo.com>
2015-06-29 10:57:34 +0000

    with two extra fields - 'date' and 'active'
    git-svn-id: http://192.168.1.10/repos/toto@1304 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/admin/onlinereport/OnlineReportDAO.java
+++ Toto/trunk/src/com/azquo/admin/onlinereport/OnlineReportDAO.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +24,7 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
     }
 
     // column names except ID which is in the superclass
+    public static final String  DATECREATED = "date_created";
 
     public static final String BUSINESSID = "business_id";
     public static final String DATABASEID = "database_id";
@@ -33,11 +36,13 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
     public static final String FILENAME = "filename";
     public static final String EXPLANATION = "explanation";
     public static final String RENDERER = "renderer";
+    public static final String ACTIVE = "active";
 
     @Override
     public Map<String, Object> getColumnNameValueMap(final OnlineReport onlineReport) {
         final Map<String, Object> toReturn = new HashMap<String, Object>();
         toReturn.put(ID, onlineReport.getId());
+        toReturn.put(DATECREATED,  Date.from(onlineReport.getDateCreated().atZone(ZoneId.systemDefault()).toInstant()));
         toReturn.put(BUSINESSID, onlineReport.getBusinessId());
         toReturn.put(DATABASEID, onlineReport.getDatabaseId());
         toReturn.put(REPORTNAME, onlineReport.getReportName());
@@ -46,15 +51,17 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
         toReturn.put(FILENAME, onlineReport.getFilename());
         toReturn.put(EXPLANATION, onlineReport.getExplanation());
         toReturn.put(RENDERER, onlineReport.getRenderer());
+        toReturn.put(ACTIVE, onlineReport.getActive());
         return toReturn;
     }
 
-    public static final class OnlineReportRowMapper implements RowMapper<OnlineReport> {
+    public final class OnlineReportRowMapper implements RowMapper<OnlineReport> {
 
         @Override
         public OnlineReport mapRow(final ResultSet rs, final int row) throws SQLException {
             try {
                 return new OnlineReport(rs.getInt(ID)
+                        , getLocalDateTimeFromDate(rs.getDate(DATECREATED))
                         , rs.getInt(BUSINESSID)
                         , rs.getInt(DATABASEID)
                         , ""
@@ -64,7 +71,8 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
                         , rs.getString(FILENAME)
                         , ""
                         , rs.getString(EXPLANATION)
-                        , rs.getInt(RENDERER));
+                        , rs.getInt(RENDERER)
+                        , rs.getBoolean(ACTIVE));
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
@@ -81,6 +89,7 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
         final MapSqlParameterSource namedParams = new MapSqlParameterSource();
         namedParams.addValue(DATABASEID, databaseId);
         namedParams.addValue(REPORTNAME, reportName);
+        namedParams.addValue(ACTIVE, true);
         return findOneWithWhereSQLAndParameters(" WHERE `" + DATABASEID + "` = :" + DATABASEID + " and `" + REPORTNAME + "` = :" + REPORTNAME, namedParams);
     }
 
@@ -99,20 +108,23 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
 
         namedParams.addValue(BUSINESSID, businessId);
         namedParams.addValue(USERSTATUS, "%" + userStatus + "%");
-        return findListWithWhereSQLAndParameters(" WHERE `" + BUSINESSID + "` = :" + BUSINESSID + " and " + statusSelect + " order by " + REPORTCATEGORY + ", " + REPORTNAME, namedParams, false);
+        namedParams.addValue(ACTIVE, true);
+        return findListWithWhereSQLAndParameters(" WHERE `" + BUSINESSID + "` = :" + BUSINESSID + " and " + ACTIVE + " = :" + ACTIVE + " and " + statusSelect + " order by " + REPORTCATEGORY + ", " + REPORTNAME, namedParams, false);
     }
 
     public List<OnlineReport> findForBusinessId(final int businessId) {
         final MapSqlParameterSource namedParams = new MapSqlParameterSource();
         namedParams.addValue(BUSINESSID, businessId);
-        return findListWithWhereSQLAndParameters("WHERE " + BUSINESSID + " = :" + BUSINESSID, namedParams, false);
+        namedParams.addValue(ACTIVE, true);
+        return findListWithWhereSQLAndParameters("WHERE " + BUSINESSID + " = :" + BUSINESSID + " and " + ACTIVE + " = :" + ACTIVE, namedParams, false);
     }
 
 
     public List<OnlineReport> findForDatabaseId(final int databaseId) {
         final MapSqlParameterSource namedParams = new MapSqlParameterSource();
         namedParams.addValue(DATABASEID, databaseId);
-        return findListWithWhereSQLAndParameters("WHERE " + DATABASEID + " = :" + DATABASEID, namedParams, false);
+        namedParams.addValue(ACTIVE, true);
+        return findListWithWhereSQLAndParameters("WHERE " + DATABASEID + " = :" + DATABASEID + " and " + ACTIVE + " = :" + ACTIVE, namedParams, false);
     }
 
     public void removeForDatabaseId(int databaseId) {
@@ -120,6 +132,9 @@ public class OnlineReportDAO extends StandardDAO<OnlineReport> {
         namedParams.addValue(DATABASEID, databaseId);
         jdbcTemplate.update("DELETE FROM " + MASTER_DB + ".`" + getTableName() + "` where " + DATABASEID + " = :" + DATABASEID, namedParams);
     }
+
+
+
 }
 
 
