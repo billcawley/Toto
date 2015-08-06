012988f0b08a3a98804976d7b0d41704b4ee3424
Bill Cawley <bill@azquo.com>
2015-08-06 08:42:41 +0000

    controller for new page to show data lists
    git-svn-id: http://192.168.1.10/repos/toto@1412 b53939d4-f08c-4181-96d4-b201aa9c61c4

---
+++ Toto/trunk/src/com/azquo/admin/controller/ShowdataController.java
@@ -0,0 +1,48 @@
+package com.azquo.admin.controller;
+
+import com.azquo.memorydb.TreeNode;
+import com.azquo.spreadsheet.LoggedInUser;
+import com.azquo.spreadsheet.SpreadsheetService;
+import com.azquo.spreadsheet.controller.LoginController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by bill on 05/08/15.
+ */
+
+@Controller
+@RequestMapping("/Showdata")
+
+public class ShowdataController {
+
+    @Autowired
+    SpreadsheetService spreadsheetService;
+
+      @RequestMapping
+    public String handleRequest(HttpServletRequest request
+            , @RequestParam(value = "chosen", required = false) String chosen
+    ) throws Exception
+
+    {
+        LoggedInUser loggedInUser = (LoggedInUser) request.getSession().getAttribute(LoginController.LOGGED_IN_USER_SESSION);
+        if (loggedInUser == null || !loggedInUser.getUser().isAdministrator()) {
+            return "redirect:/api/Login";
+        } else {
+            TreeNode node = spreadsheetService.getTreeNode(loggedInUser, chosen);
+            request.setAttribute("node", node);
+            //model.put("node", node);
+            return "showdata";
+        }
+    }
+}
+
+
+
