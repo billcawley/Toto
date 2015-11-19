3b9b7f7f154a7a02363b97dc63c3077b2a59af3f
Bill Cawley <bill@azquo.com>
2015-11-19 11:24:04 +0000

    gives precedence to user options on spreadsheet to those stored in table
    git-svn-id: http://192.168.1.10/repos/toto@1636 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/spreadsheet/view/ZKAzquoBookUtils.java
+++ Toto/trunk/src/com/azquo/spreadsheet/view/ZKAzquoBookUtils.java
@@ -109,15 +109,29 @@ public class ZKAzquoBookUtils {
                 }
                 if (name.getName().startsWith(azDataRegion)) { // then we have a data region to deal with here
                     String region = name.getName().substring(azDataRegion.length()); // might well be an empty string
-                    UserRegionOptions userRegionOptions = userRegionOptionsDAO.findForUserIdReportIdAndRegion(loggedInUser.getUser().getId(), reportId, region);
-                    if (userRegionOptions == null) { // then get one from the sheet if we can
-                        String source = "";
-                        CellRegion optionsRegion = getCellRegionForSheetAndName(sheet, azOptions + region);
-                        if (optionsRegion != null) {
-                            source = sheet.getInternalSheet().getCell(optionsRegion.getRow(), optionsRegion.getColumn()).getStringValue();
-                        }
-                        userRegionOptions = new UserRegionOptions(0, loggedInUser.getUser().getId(), reportId, region, source);
+                    UserRegionOptions userRegionOptions = null;
+                    CellRegion optionsRegion = getCellRegionForSheetAndName(sheet, azOptions + region);
+                    String source = "";
+                    if (optionsRegion != null) {
+                        source = sheet.getInternalSheet().getCell(optionsRegion.getRow(), optionsRegion.getColumn()).getStringValue();
                     }
+                    userRegionOptions = new UserRegionOptions(0, loggedInUser.getUser().getId(), reportId, region, source);
+
+
+                    UserRegionOptions userRegionOptions2 = userRegionOptionsDAO.findForUserIdReportIdAndRegion(loggedInUser.getUser().getId(), reportId, region);
+                    //only these five fields to be taken from the table.   other fields - hide_rows, sortable, row_limit, column_limit should be removed from SQL table
+                    if (userRegionOptions2!=null) {
+                        if (userRegionOptions.getSortColumn() == null) {
+                            userRegionOptions.setSortColumn(userRegionOptions2.getSortColumn());
+                            userRegionOptions.setSortColumnAsc(userRegionOptions2.getSortColumnAsc());
+                        }
+                        if (userRegionOptions.getSortRow() == null) {
+                            userRegionOptions.setSortRow(userRegionOptions2.getSortRow());
+                            userRegionOptions.setSortRowAsc(userRegionOptions2.getSortRowAsc());
+                        }
+                        userRegionOptions.setHighlightDays(userRegionOptions2.getHighlightDays());
+                    }
+
                     fillRegion(sheet, region, userRegionOptions, loggedInUser);
                 }
             }
