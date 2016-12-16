64e69bf46f941baeeb153e116c44bb8b894e58c7
Bill Cawley <bill@azquo.com>
2016-12-16 13:31:17 +0000

    allows drilldown without 'with'
    git-svn-id: http://192.168.1.10/repos/toto@2251 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/spreadsheet/SpreadsheetService.java
+++ Toto/trunk/src/com/azquo/spreadsheet/SpreadsheetService.java
@@ -203,7 +203,7 @@ public class SpreadsheetService {
         if (inSpreadPos < 0) return null;
         int withPos = provline.indexOf(" with ", inSpreadPos);
         if (withPos < 0) withPos = provline.indexOf(".");//no parameters
-        if (withPos < 0) return null;
+        if (withPos < 0) withPos = provline.length();//still no parameters!
         String reportName = provline.substring(inSpreadPos + 14, withPos).trim().replace("`", "");//strip any spurious `
         String paramString = provline.substring(withPos + 6);
         int equalsPos = paramString.indexOf(" = ");
