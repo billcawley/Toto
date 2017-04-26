22fbce4eb55739d57b0bec2d3268e9069d9437b8
Bill Cawley <bill@azquo.com>
2017-04-26 16:11:08 +0000

    when looking for parent attribute will use direct connection if it exists
    git-svn-id: http://192.168.1.10/repos/toto@2402 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/DatabaseServer/src/com/azquo/memorydb/core/Name.java
+++ Toto/trunk/DatabaseServer/src/com/azquo/memorydb/core/Name.java
@@ -812,8 +812,8 @@ public final class Name extends AzquoMemoryDBEntity {
     // criteria for fall back attributes added by WFC, not entirely sure I'd have done this but anyway
     private static AtomicInteger findParentAttributesCount = new AtomicInteger(0);
 
-    private static String findParentAttributes(Name child, String attributeName, Set<Name> checked) {
-        findParentAttributesCount.incrementAndGet();
+    private static String findParentAttributes(Name child, String attributeName, Set<Name> checked, Name origName, int level) {
+         findParentAttributesCount.incrementAndGet();
         attributeName = attributeName.trim().toUpperCase();
         for (Name parent : child.parents) {
             if (parent == null) {
@@ -829,32 +829,36 @@ public final class Name extends AzquoMemoryDBEntity {
                             // now check the parents to see if it's correct
                             for (Name parentParent : parent.getParents()){ // yes parent parent a bit hacky, we're looking to qualify the parent. getParents not idea for garbage but I assume this will NOT be called that often!
                                 if (parentParent.getDefaultDisplayName().equalsIgnoreCase(parentName)){
+                                    if (level > 1) {
+                                        //check that there is not a direct connection.
+                                        Set<Name> directConnection = new HashSet(origName.getParents());
+                                        directConnection.retainAll(parent.getChildren());
+                                        if (directConnection.size() > 0) {
+                                            return directConnection.iterator().next().getDefaultDisplayName();
+                                        }
+                                    }
                                     return child.getDefaultDisplayName();
                                 }
                             }
                         }
                     } else { // normal
                         if (parent.getDefaultDisplayName() != null && parent.getDefaultDisplayName().equalsIgnoreCase(attributeName)) {
+                            if (level > 1) {
+                                //check that there is not a direct connection.
+                                Set<Name> directConnection = new HashSet(origName.getParents());
+                                directConnection.retainAll(parent.getChildren());
+                                if (directConnection.size() > 0) {
+                                    return directConnection.iterator().next().getDefaultDisplayName();
+                                }
+                            }
                             return child.getDefaultDisplayName();
                         }
                     }
 
-                    String attribute = parent.getAttribute(attributeName, false, null); // no parent check first time
+                    String attribute = parent.getAttribute(attributeName, true, checked, origName, level + 1); // no parent check first time
                     if (attribute != null) {
                         return attribute;
                     }
-                }
-            }
-        }
-        // first loop doesn't do parent check (doesn't recurse).
-        // The issue here is that there may be early on a match but some distance up the parent hierarchy when a subsequent parent lower down matches more closely
-        // perhaps a direct parent. So run through the parents looking for a direct parent attribute match and if you can't find it then try again but allow going up a level
-        // Might cause performance issues - if so I'm not 100% on how easy it will be to optimise gracefully. Of course this is only an issue when there's not an attribute found easily
-        for (Name parent : child.parents) {
-            if (parent != null) {
-                String attribute = parent.getAttribute(attributeName, true, checked); // second time go up . . .
-                if (attribute != null) {
-                    return attribute;
                 }
             }
         }
@@ -873,13 +877,19 @@ public final class Name extends AzquoMemoryDBEntity {
     private static AtomicInteger getAttribute2Count = new AtomicInteger(0);
 
     public String getAttribute(String attributeName, boolean parentCheck, Set<Name> checked) {
+
+        return getAttribute(attributeName, parentCheck, checked, this, 0);
+    }
+
+
+        public String getAttribute(String attributeName, boolean parentCheck, Set<Name> checked, Name origName, int level) {
         attributeName = attributeName.toUpperCase(); // edd adding (back?) in, need to do this since all attributes are uppercase internally - check there are not redundant uppercases in other places TODO
         getAttribute2Count.incrementAndGet();
         String attribute = nameAttributes.getAttribute(attributeName);
         if (attribute != null) return attribute;
         //look up the chain for any parent with the attribute
         if (parentCheck) {
-            return findParentAttributes(this, attributeName, checked);
+            return findParentAttributes(this, attributeName, checked, origName, level);
         }
         return null;
     }
