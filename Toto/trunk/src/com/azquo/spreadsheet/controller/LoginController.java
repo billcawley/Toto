5d2a1f712ffbac369b80621db72e24bd8fa3764a
Bill Cawley <bill@azquo.com>
2017-11-14 17:03:48 +0000

    Will return information in Json
    git-svn-id: http://192.168.1.10/repos/toto@2599 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/spreadsheet/controller/LoginController.java
+++ Toto/trunk/src/com/azquo/spreadsheet/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Copyright (C) 2016 Azquo Ltd. Public source releases are under the AGPLv3, see LICENSE.TXT
@@ -33,11 +34,11 @@ public class LoginController {
     public static final String LOGGED_IN_USER_SESSION = "LOGGED_IN_USER_SESSION";
 
     @RequestMapping
-    public String handleRequest(ModelMap model, HttpServletRequest request
+    public String handleRequest(ModelMap model, HttpServletRequest request, HttpServletResponse response
             , @RequestParam(value = "user", required = false) String userEmail
             , @RequestParam(value = "password", required = false) String password
             , @RequestParam(value = "logoff", required = false) String logoff
-            , @RequestParam(value = "connectionid", required = false) String connectionid // only for the magento plugin
+            , @RequestParam(value = "connectionid", required = false) String connectionid // only for the magento plugin and Javascript (connectionId = "javascript")
     ) throws Exception {
         if ("true".equals(logoff)) {
             if (request.getSession().getAttribute(LOGGED_IN_USER_SESSION) != null) {
@@ -55,7 +56,7 @@ public class LoginController {
                 request.getSession().removeAttribute(LOGGED_IN_USER_SESSION);
             }
         }
-        if (connectionid != null && connectionid.length() > 0) { // nasty hack to support connection id from the plugin
+        if (connectionid != null && connectionid.length() > 0 && !connectionid.equals("javascript")) { // nasty hack to support connection id from the plugin
             if (request.getServletContext().getAttribute(connectionid) != null) { // then pick up the temp logged in conneciton
                 LoggedInUser loggedInUser = (LoggedInUser) request.getServletContext().getAttribute(connectionid);
                 request.getSession().setAttribute(LOGGED_IN_USER_SESSION, loggedInUser);
@@ -83,13 +84,43 @@ public class LoginController {
                         AzquoMailer.sendEMail("bruce.cooper@azquo.com", "Bruce", title, userAgent);
                     }
                     request.getSession().setAttribute(LOGGED_IN_USER_SESSION, loggedInUser);
-                    if (loggedInUser.getUser().isAdministrator() || loggedInUser.getUser().isDeveloper()) {
-                        return "redirect:/api/ManageReports";
+                    if (connectionid != null && connectionid.equals("javascript")) {
+                        String jsonFunction = "azquojsonresponse";
+                        String userType = "user";
+                        if (loggedInUser.getUser().isAdministrator()){
+                            userType = "administrator";
+
+                        }else{
+                            if (loggedInUser.getUser().isDeveloper()){
+                                userType = "developer";
+
+                            }else{
+                                if (loggedInUser.getUser().isMaster()){
+                                    userType = "master";
+                                }
+                            }
+                        }
+                         model.addAttribute("content", jsonFunction + "({\"usertype\":\"" + userType + "})");
+                        return "utf8page";
+
                     } else {
-                        return "redirect:/api/Online?reportid=1"; // redirect to menu, will need to be changed when we sort the parameters out
+                        if (loggedInUser.getUser().isAdministrator() || loggedInUser.getUser().isDeveloper()) {
+                            return "redirect:/api/ManageReports";
+                        } else {
+                            return "redirect:/api/Online?reportid=1"; // redirect to menu, will need to be changed when we sort the parameters out
+                        }
                     }
                 } else {// feedback to users about incorrect details
-                    model.put("error", "incorrect login details");
+                    if (connectionid != null && connectionid.equals("javascript")) {
+                        String jsonFunction = "azquojsonresponse";
+                        model.addAttribute("content", jsonFunction + "({\"usertype\":\"failed\"})");
+                        response.setHeader("Access-Control-Allow-Origin", "*");
+                        response.setHeader("Content-type", "application/json");
+                        return "utf8page";
+
+                    } else {
+                        model.put("error", "incorrect login details");
+                    }
                 }
             }
         }
