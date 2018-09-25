67066d2f58026fcbd32b0cc00d7dd114f92f22a8
Bill Cawley <bill@azquo.com>
2018-09-25 14:27:17 +0000

    does not fill chosen cells in data regions
    git-svn-id: http://192.168.1.10/repos/toto@2809 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/spreadsheet/zk/ChoicesService.java
+++ Toto/trunk/src/com/azquo/spreadsheet/zk/ChoicesService.java
@@ -96,12 +96,16 @@ public class ChoicesService {
                                         row++;// like starting at 1
                                         SCell vCell = validationSheet.getInternalSheet().getCell(row, numberOfValidationsAdded);
                                         vCell.setCellStyle(sheet.getInternalSheet().getCell(chosenRegion.getRow(), chosenRegion.getColumn()).getCellStyle());
+                                        /*
+                                        //don't set values in the data region
                                         try {
                                             Date date = df.parse(choiceOption);
                                             vCell.setDateValue(date);
                                         } catch (Exception e) {
                                             BookUtils.setValue(vCell, choiceOption);
                                         }
+                                        */
+
                                     }
                                     if (row > 0) { // if choice options is empty this will not work
                                         Range validationValues = Ranges.range(validationSheet, 1, numberOfValidationsAdded, row, numberOfValidationsAdded);
@@ -179,7 +183,8 @@ public class ChoicesService {
             context = "";
             // Now I need to run through all choices setting from the user options IF it is valid and the first on the menu if it is not
             for (SName sName : namesForSheet) {
-                if (sName.getName().endsWith("Chosen")) {
+                 boolean inDataRegion =  !BookUtils.getNamedDataRegionForRowAndColumnSelectedSheet(sName.getRefersToCellRegion().getRow(), sName.getRefersToCellRegion().getColumn(),sheet).isEmpty();
+                if (sName.getName().endsWith("Chosen") && !inDataRegion) {
                     CellRegion chosen = sName.getRefersToCellRegion();
                     String choiceName = sName.getName().substring(0, sName.getName().length() - "Chosen".length()).toLowerCase();
                     if (chosen != null) {
