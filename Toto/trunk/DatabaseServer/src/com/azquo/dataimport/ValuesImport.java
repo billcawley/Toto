0d00421e4803874b2573562a20c817cb7c52f361
Bill Cawley <bill@azquo.com>
2019-03-12 19:06:13 +0000

    Continues on if the CSV parser finds a corrupt line.
    git-svn-id: http://192.168.1.10/repos/toto@3115 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/DatabaseServer/src/com/azquo/dataimport/ValuesImport.java
+++ Toto/trunk/DatabaseServer/src/com/azquo/dataimport/ValuesImport.java
@@ -30,6 +30,7 @@ public class ValuesImport {
     static void valuesImport(AzquoMemoryDBConnection connection, MappingIterator<String[]> lineIterator
             , UploadedFile uploadedFile, List<ImmutableImportHeading> importHeadings
             , int batchSize, int lastColumnToActuallyRead, CompositeIndexResolver compositeIndexResolver) {
+        ArrayList<UploadedFile.RejectedLine> rejectedLinesForUserFeedback = new ArrayList<>();
         try {
             long track = System.currentTimeMillis();
             // now, since this will be multi threaded need to make line objects to batch up. Cannot be completely immutable due to the current logic e.g. composite values
@@ -40,70 +41,77 @@ public class ValuesImport {
             // new format. Line number, the line itself and then a list of errors
             final Map<Integer, List<String>> linesRejected = new ConcurrentHashMap<>(); // track line numbers rejected
             int linesImported = 0; // just for some feedback at the end
+            int lineNo = 0;
             while (lineIterator.hasNext()) { // the main line reading loop
-                String[] lineValues = lineIterator.next();
-                // I'm currently working on the principle that the line no is the current location -1
-                // while as rejected lines could well be sequential!
-                while (uploadedFile.getIgnoreLines() != null && uploadedFile.getIgnoreLines().keySet().contains(lineIterator.getCurrentLocation().getLineNr() - 1) && lineIterator.hasNext()){
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("Deliberately skipping line " + (lineIterator.getCurrentLocation().getLineNr() - 1) + ", ");
-                    for (String cell : lineValues){
-                        sb.append("\t").append(cell);
-                    }
-                    System.out.println(sb.toString());
-                    uploadedFile.getIgnoreLinesValues().put(lineIterator.getCurrentLocation().getLineNr() - 1, sb.toString());
-                    lineValues = lineIterator.next();
-                }
-
-                linesImported++;
-                List<ImportCellWithHeading> importCellsWithHeading = new ArrayList<>();
-                int columnIndex = 0;
+                lineNo++;
                 boolean corrupt = false;
-                boolean blankLine = true;
+                String[] lineValues;
+                try {
+                    lineValues = lineIterator.next();
+                    // I'm currently working on the principle that the line no is the current location -1
+                    // while as rejected lines could well be sequential!
+                    while (uploadedFile.getIgnoreLines() != null && uploadedFile.getIgnoreLines().keySet().contains(lineIterator.getCurrentLocation().getLineNr() - 1) && lineIterator.hasNext()) {
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("Deliberately skipping line " + (lineIterator.getCurrentLocation().getLineNr() - 1) + ", ");
+                        for (String cell : lineValues) {
+                            sb.append("\t").append(cell);
+                        }
+                        System.out.println(sb.toString());
+                        uploadedFile.getIgnoreLinesValues().put(lineIterator.getCurrentLocation().getLineNr() - 1, sb.toString());
+                        lineValues = lineIterator.next();
+                    }
+
+                    linesImported++;
+                    List<ImportCellWithHeading> importCellsWithHeading = new ArrayList<>();
+                    int columnIndex = 0;
+                      boolean blankLine = true;
                 /* essentially run some basic checks on the data as it comes in and batch it up into headings with line values.
                 that pairing is very helpful in the BatchImporter
                 */
-                int cellCount = 0;
-                for (ImmutableImportHeading immutableImportHeading : importHeadings) {
-                    // Intern may save a little memory if strings are repeated a lot. Column Index could point past line values for things like composite.
-                    String lineValue = (cellCount++ <= lastColumnToActuallyRead && columnIndex < lineValues.length) ? lineValues[columnIndex].trim().intern() : "";
-                    if (lineValue.length() > 0) blankLine = false;
-                    if (lineValue.equals("\"")) {// was a problem, might be worth checking if it is still
-                        corrupt = true;
-                        break;
+                    int cellCount = 0;
+                    for (ImmutableImportHeading immutableImportHeading : importHeadings) {
+                        // Intern may save a little memory if strings are repeated a lot. Column Index could point past line values for things like composite.
+                        String lineValue = (cellCount++ <= lastColumnToActuallyRead && columnIndex < lineValues.length) ? lineValues[columnIndex].trim().intern() : "";
+                        if (lineValue.length() > 0) blankLine = false;
+                        if (lineValue.equals("\"")) {// was a problem, might be worth checking if it is still
+                            corrupt = true;
+                            break;
+                        }
+                        if (lineValue.startsWith("\"") && lineValue.endsWith("\"")) {
+                            lineValue = lineValue.substring(1, lineValue.length() - 1).replace("\"\"", "\"");//strip spurious quote marks inserted by Excel
+                        }
+                        if (lineValue.startsWith("'") && lineValue.indexOf("'", 1) < 0) {
+                            lineValue = lineValue.substring(1);//in Excel insertion of an initial ' means it is a string.
+                        }
+                        // if generated from a spreadsheet this is a danger, fix now before any interpreting
+                        //.replace("\n", "\\\\n").replace("\t", "\\\\t") is what that function did on the report server.
+                        if (uploadedFile.isConvertedFromWorksheet()) {
+                            lineValue = lineValue.replace("\\\\t", "\t").replace("\\\\n", "\n");
+                        }
+                        lineValue = checkNumeric(lineValue);
+                        importCellsWithHeading.add(new ImportCellWithHeading(immutableImportHeading, lineValue));
+                        columnIndex++;
                     }
-                    if (lineValue.startsWith("\"") && lineValue.endsWith("\"")) {
-                        lineValue = lineValue.substring(1, lineValue.length() - 1).replace("\"\"", "\"");//strip spurious quote marks inserted by Excel
-                    }
-                    if (lineValue.startsWith("'") && lineValue.indexOf("'", 1) < 0) {
-                        lineValue = lineValue.substring(1);//in Excel insertion of an initial ' means it is a string.
-                    }
-                    // if generated from a spreadsheet this is a danger, fix now before any interpreting
-                    //.replace("\n", "\\\\n").replace("\t", "\\\\t") is what that function did on the report server.
-                    if (uploadedFile.isConvertedFromWorksheet()) {
-                        lineValue = lineValue.replace("\\\\t", "\t").replace("\\\\n", "\n");
-                    }
-                    lineValue = checkNumeric(lineValue);
-                    importCellsWithHeading.add(new ImportCellWithHeading(immutableImportHeading, lineValue));
-                    columnIndex++;
-                }
-                if (!corrupt && !blankLine) {
-                    linesBatched.add(new TypedPair<>(lineIterator.getCurrentLocation().getLineNr() - 1,importCellsWithHeading)); // line no - 1 as we want where it was, not where it's waiting now
-                    // Start processing this batch. As the file is read the active threads will rack up to the maximum number allowed rather than starting at max. Store the futures to confirm all are done after all lines are read.
-                    // batch size is derived by getLineIteratorAndBatchSize
-                    if (linesBatched.size() == batchSize) {
-                        futureBatches.add(ThreadPools.getMainThreadPool().submit(
+                    if (!corrupt && !blankLine) {
+                        linesBatched.add(new TypedPair<>(lineIterator.getCurrentLocation().getLineNr() - 1, importCellsWithHeading)); // line no - 1 as we want where it was, not where it's waiting now
+                        // Start processing this batch. As the file is read the active threads will rack up to the maximum number allowed rather than starting at max. Store the futures to confirm all are done after all lines are read.
+                        // batch size is derived by getLineIteratorAndBatchSize
+                        if (linesBatched.size() == batchSize) {
+                            futureBatches.add(ThreadPools.getMainThreadPool().submit(
 
-                                new BatchImporter(connection
-                                        , uploadedFile.getNoValuesAdjusted(), linesBatched
-                                        , namesFoundCache, uploadedFile.getLanguages()
-                                        , linesRejected, uploadedFile.getParameter("cleardata") != null, compositeIndexResolver))// line no should be the start
+                                    new BatchImporter(connection
+                                            , uploadedFile.getNoValuesAdjusted(), linesBatched
+                                            , namesFoundCache, uploadedFile.getLanguages()
+                                            , linesRejected, uploadedFile.getParameter("cleardata") != null, compositeIndexResolver))// line no should be the start
 
-                        );
-                        linesBatched = new ArrayList<>(batchSize);
+                            );
+                            linesBatched = new ArrayList<>(batchSize);
+                        }
+                    } else {
+                        linesImported--; // drop it down as we're not even going to try that line
                     }
-                } else {
-                    linesImported--; // drop it down as we're not even going to try that line
+                }catch (Exception e){
+                    rejectedLinesForUserFeedback.add(new UploadedFile.RejectedLine(lineNo, "", e.getMessage()));
                 }
             }
             futureBatches.add(ThreadPools.getMainThreadPool().submit(new BatchImporter(connection
@@ -129,11 +137,10 @@ public class ValuesImport {
             if (!linesRejected.isEmpty()) {
                 // I'm going to have to go through the file and fine the rejected lines if they're there, can't really use the iterator before I don't think.
                 // I guess watch for possible performance issues . . .
-                ArrayList<UploadedFile.RejectedLine> rejectedLinesForUserFeedback = new ArrayList<>();
-                // this try should gracefully release the resources
+                 // this try should gracefully release the resources
                 long time = System.currentTimeMillis();
                 try (BufferedReader br = Files.newBufferedReader(Paths.get(uploadedFile.getPath()), Charset.forName("UTF-8"))) {
-                    int lineNo = 0;
+                    lineNo = 0;
                     String line;
                     // I assume the line no as given by the json iterator starts at line 1, might have to check this . . .
                     while ((line = br.readLine()) != null){
