6dec8c50cedd46e9c34b51be317b12f851f5cd25
Bill Cawley <bill@azquo.com>
2019-08-02 17:46:37 +0000

    recognises 'import templates' anywhere in the file name
    git-svn-id: http://192.168.1.10/repos/toto@3244 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/admin/controller/ManageDatabasesController.java
+++ Toto/trunk/src/com/azquo/admin/controller/ManageDatabasesController.java
@@ -326,7 +326,7 @@ public class ManageDatabasesController {
                                 }
                             }
                             //detect from workbook name
-                            if (uploadedFile.getFileName().toLowerCase().endsWith("import templates.xlsx")) {
+                            if (uploadedFile.getFileName().toLowerCase().contains("import templates")) {
                                 isImportTemplate = true;
                             }
                             // detect Ben Jones contract style template
