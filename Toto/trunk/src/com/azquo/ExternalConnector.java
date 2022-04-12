2a3beef214bc942876d274f06476d9aa5e10bf31
Bill Cawley <bill@azquo.com>
2022-04-12 15:51:09 +0000

    Includes a 'save' routine that needs adjusting for prepared statements.
    git-svn-id: http://192.168.1.10/repos/toto@4033 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/ExternalConnector.java
+++ Toto/trunk/src/com/azquo/ExternalConnector.java
@@ -10,32 +10,37 @@ for the moment I'll just use this class
  */
 
 import com.azquo.admin.AdminService;
-import com.azquo.admin.StandardDAO;
 import com.azquo.admin.onlinereport.ExternalDatabaseConnection;
 import com.azquo.spreadsheet.LoggedInUser;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 
 public class ExternalConnector {
 
 
-
-    public static List<List<String>> getData(LoggedInUser loggedInUser, String connectionName, String query) {
+    public static List<List<String>> getData(LoggedInUser loggedInUser, String connectionName, String query, Map<String, String> valueMap, String keyField) {
         List<List<String>> toReturn = new ArrayList<>();
         // intial test code hard coded to snowflake
         Connection con = null;
         try {
             ExternalDatabaseConnection externalDatabaseConnection = AdminService.getExternalDatabaseConnectionByName(connectionName, loggedInUser);
-            if (externalDatabaseConnection != null){
-                if (externalDatabaseConnection.getConnectionString().startsWith("jdbc:snowflake")){
+            if (externalDatabaseConnection != null) {
+                if (externalDatabaseConnection.getConnectionString().startsWith("jdbc:snowflake")) {
                     Class.forName("com.snowflake.client.jdbc.SnowflakeDriver");
                     con = DriverManager.getConnection(externalDatabaseConnection.getConnectionString(), externalDatabaseConnection.getUser(), externalDatabaseConnection.getPassword());
 
+                    if (valueMap != null || keyField!=null) {
+                        save(externalDatabaseConnection, con, query, valueMap, keyField);
+                        return null;
+                    }
                     //con = StandardDAO.sfDataSource.getConnection();
                     Statement stat = con.createStatement();
-                    if (externalDatabaseConnection.getDatabase() != null && !externalDatabaseConnection.getDatabase().isEmpty()){
+                    if (externalDatabaseConnection.getDatabase() != null && !externalDatabaseConnection.getDatabase().isEmpty()) {
                         stat.executeQuery("use database " + externalDatabaseConnection.getDatabase());
                     }
                     ResultSet res = stat.executeQuery(query);
@@ -43,14 +48,18 @@ public class ExternalConnector {
                     int columnCount = rsmd.getColumnCount();
                     List<String> headings = new ArrayList<>();
                     // The column count starts from 1
-                    for (int i = 1; i <= columnCount; i++ ) {
+                    for (int i = 1; i <= columnCount; i++) {
                         headings.add(rsmd.getColumnName(i));
                     }
                     toReturn.add(headings);
-                    while (res.next()){
+                    while (res.next()) {
                         List<String> line = new ArrayList<>();
-                        for (String heading : headings){
-                            line.add(res.getObject(heading).toString());
+                        for (String heading : headings) {
+                            try {
+                                line.add(res.getObject(heading).toString());
+                            }catch(Exception e){
+                                line.add("");
+                            }
                         }
                         toReturn.add(line);
                     }
@@ -60,7 +69,7 @@ public class ExternalConnector {
 
             }
         } catch (SQLException | ClassNotFoundException throwables) {
-            if (con != null){
+            if (con != null) {
                 try {
                     con.close();
                 } catch (SQLException e) {
@@ -72,4 +81,79 @@ public class ExternalConnector {
         return toReturn;
     }
 
+
+    public static void save(ExternalDatabaseConnection externalDatabaseConnection, Connection con, String query, Map<String, String> valueMap, String keyField) throws SQLException {
+        List<String> params = new ArrayList<>();
+        // NOTE THIS IS UNSAFE - I can't work out how to use prepared statements if they need to be preceded by 'USE DATABASE'
+
+        if (!query.toLowerCase(Locale.ROOT).startsWith("update ")) {
+            //UPDATE holds the file name if it is not a SQL instruction
+            StringBuffer newQuery = new StringBuffer();
+            if (valueMap.size()==1){
+                newQuery.append("delete from " + query);
+                newQuery.append(" where \"" + keyField + "\" = " + valueMap.get(keyField));
+            }else if (keyField == null){  // inserting
+               // insert <filename> -> insert into <filename> a=1,b=2,etc
+                newQuery.append("insert into ");
+                newQuery.append(query);
+                newQuery.append(" (");
+                int i=0;
+                for (String fieldName : valueMap.keySet()) {
+                    newQuery.append("\"" + fieldName + "\"");
+                    if (++i < valueMap.size()) {
+                        newQuery.append(",");
+                    }
+                }
+                newQuery.append(") VALUES (");
+                i= 0;
+                for (String fieldName : valueMap.keySet()) {
+                    newQuery.append(valueMap.get(fieldName));
+                    if (++i < valueMap.size()) {
+                        newQuery.append(",");
+                    }
+                }
+                newQuery.append(")");
+            }else {
+                //update all fields
+                newQuery.append("update ");
+                newQuery.append(query);
+
+                newQuery.append(" set ");
+                int i = 0;
+                for (String fieldName : valueMap.keySet()) {
+                    newQuery.append("\"" + fieldName + "\"");
+                    newQuery.append(" = ");
+                    newQuery.append(valueMap.get(fieldName));
+                    if (++i < valueMap.size()) {
+                        newQuery.append(",");
+                    } else {
+                        newQuery.append(" WHERE ");
+                        newQuery.append("\"" + keyField + "\"");
+                        newQuery.append(" = ");
+                        newQuery.append(valueMap.get(keyField));
+                    }
+                }
+            }
+            query = newQuery.toString();
+        }else{
+            int cursorPos = 0;
+            while (query.substring(cursorPos).contains(":")) {
+                int colonPos = query.indexOf(":", cursorPos);
+                int endField = query.indexOf(" ", colonPos + 2);
+                if (endField < 0) {
+                    endField = query.length();
+                }
+                String fieldName = query.substring(colonPos + 1, endField).trim();
+                String fieldValue = valueMap.get(fieldName);
+                if (fieldValue == null) {
+                    throw new SQLException("no field named: " + fieldName);
+                }
+                params.add(fieldValue);
+                query = query.substring(0, colonPos) + fieldValue + query.substring(endField);
+                cursorPos = colonPos + fieldValue.length();
+            }
+        }
+        Statement stat = con.createStatement();
+        ResultSet res = stat.executeQuery(query);
+    }
 }
