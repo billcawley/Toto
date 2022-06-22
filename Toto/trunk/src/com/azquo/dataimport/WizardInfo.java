f0d96d1075e9059ab721862608f78b679942afb0
Bill Cawley <bill@azquo.com>
2022-06-22 11:57:30 +0000

    Suggestions added
    git-svn-id: http://192.168.1.10/repos/toto@4107 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/src/com/azquo/dataimport/WizardInfo.java
+++ Toto/trunk/src/com/azquo/dataimport/WizardInfo.java
@@ -1,5 +1,6 @@
 package com.azquo.dataimport;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,8 @@ public class WizardInfo {
     Map<String, WizardField> fields;
     String lastDataField;
     int lineCount;
+    boolean hasSuggestions;
+    List<String> suggestionActions;
 
     public WizardInfo(String importFileName, String importFileData){
         this.importFileName = importFileName;
@@ -17,6 +20,8 @@ public class WizardInfo {
         this.fields = new LinkedHashMap<>();
         this.lastDataField = null;
         this.lineCount = 0;
+        this.hasSuggestions = true;
+        this.suggestionActions = new ArrayList<>();
      }
 
     public String getImportFileName() {
@@ -36,6 +41,14 @@ public class WizardInfo {
     public int getLineCount(){return lineCount; }
 
     public void setLineCount(int lineCount){this.lineCount = lineCount; }
+
+    public boolean getHasSuggestions(){return hasSuggestions; }
+
+    public void setHasSuggestions(boolean hasSuggestions){this.hasSuggestions = hasSuggestions; }
+
+    public List<String> getSuggestionActions(){return suggestionActions; }
+
+    public void setSuggestionActions(List<String>suggestionActions){this.suggestionActions = suggestionActions; }
 
 
 
