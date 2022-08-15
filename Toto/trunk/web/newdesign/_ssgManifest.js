a4c2575e4caed28d5947b498cb46df14b9b02a0f
Edd Cawley <edd@azquo.com>
2022-08-15 16:42:23 +0000

    zapping files, new ones incoming shortly
    git-svn-id: http://192.168.1.10/repos/toto@4123 b53939d4-f08c-4181-96d4-b201aa9c61c4

--- Toto/trunk/web/newdesign/_ssgManifest.js
+++
@@ -1,6 +0,0 @@
-self.__SSG_MANIFEST = new Set([
-    "\u002Fimports\u002F[id]",
-    "\u002Fimports\u002Fnew\u002F[stage]",
-    "\u002Freports\u002F[id]",
-]);
-self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB();
